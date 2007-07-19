ea2990a2b12844f747c5ee75f9039bf13ea95362
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-07-19 04:59:44 +0000

    + First draft index.php for vrmlengine WWW, right now mostly to send people to http://www.camelot.homedns.org/~michalis/
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@1815 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ htdocs/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+  "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<meta name="Author" content="Michalis Kamburelis">
+<meta name="Language" content="English">
+
+<style type="text/css"><!--
+  body { background: white
+    url(images/gcorner.jpg) no-repeat ; }
+  table.key_list { width: 90%; }
+  table.key_list th { background: #eeeee0; }
+  table.key_list td { background: #ddddd0; }
+
+  dt { font-weight: bold; }
+  div.page_footer { }
+  span.page_copyright { font-size: smaller }
+
+  span.version_number { font-size: small }
+
+  /* Adapted from my pasdoc.css */
+  table.thin_borders { border-collapse: collapse; }
+  table.thin_borders td { border: 1pt solid lightgray; padding: 0.3em; }
+  table.thin_borders th { border: 1pt solid lightgray; padding: 0.3em; }
+
+  dl.command_line_options_list > dt {
+    font-family: monospace;
+    font-weight: bold;
+  }
+
+  /* Leave default font-family for dt, and default font-weight.
+     Use span with command_line_option class inside dt. */
+  dl.command_line_options_list_custom > dt {
+    font-weight: normal;
+  }
+  span.command_line_option {
+    font-family: monospace;
+    font-weight: bold;
+  }
+
+  div.quick_links_bottom_line { text-align: center; }
+
+  div.latest_update_description {
+    margin-left: 3em;
+    border: 1px solid gray;
+    padding: 0.5em;
+  }
+
+  pre.vrml_code {
+    border: 1px solid #aaaaaa;
+    background-color: #f9f9f9;
+    padding: 0.3em;
+  }
+
+  dl.vrml_ver_differences > dt {
+    font-weight: normal;
+    font-style: italic;
+  }
+
+  table.program_image_links {
+    width: 100%;
+  }
+
+  td.program_image_link img { border: none; }
+
+  td.program_image_link {
+    background: #e8d59a;
+    width: 50%;
+    border: 1px solid black;
+    text-align: center;
+    padding: 0.5em;
+  }
+
+  .program_image_link_title { font-size: larger; }
+
+  /* Style for note and note_title shamelessly stolen from CSS
+     of the Subversion book. (from .sidebar and .sidebar .title) */
+  .note
+  {
+      border-top: dotted 1px black;
+      border-left: dotted 1px black;
+      border-right: solid 2px black;
+      border-bottom: solid 2px black;
+      background: rgb(240,220,170);
+      padding: 0 0.12in;
+      margin: 0.5in;
+  }
+
+  .note .note_title {
+    text-align: center;
+    font-size: 125%;
+  }
+--></style>
+
+<title>Kambi VRML game engine</title>
+
+</head>
+<body>
+
+<h1>Kambi VRML game engine</h1>
+
+<p>Right now the WWW pages with all the information, documentation,
+download links are on
+<a href="http://www.camelot.homedns.org/~michalis/">http://www.camelot.homedns.org/~michalis/</a>.
+We'll move to SourceForge WWW soon (I'm writing this on 2007-07-19,
+and hope to finish move by the end of this month).</p>
+
+<p>In the meantime, you're already welcome to use
+<a href="http://sourceforge.net/projects/vrmlengine">all SourceForge
+features for this project</a>,
+like <a href="http://sourceforge.net/tracker/?group_id=200653">bug trackers</a>
+and download latest code from our
+<a href="http://sourceforge.net/svn/?group_id=200653">Subversion repository</a>.
+</p>
+
+
+<hr>
+
+<div class="page_footer">
+
+<p>Services for the vrmlengine project provided by<br />
+<a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=200653&amp;type=3" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a><br />
+See also <a href="http://sourceforge.net/projects/vrmlengine">vrmlengine
+project page on SourceForge</a>.</p>
+
+</div>
+
+</body>
+</html>
