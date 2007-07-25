45aa42070c51fd8c45de5d97bd93885563b096e3
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-07-25 19:46:59 +0000

    * check_sums_md5.sh omits OK lines
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@1851 afe8dcc5-9094-451b-8773-5f495042a737

--- scripts/shell.sf.net/check_sums_md5.sh
+++ scripts/shell.sf.net/check_sums_md5.sh
@@ -4,5 +4,4 @@ set -ue
 # This script ignores current dir.
 
 cd /home/groups/v/vr/vrmlengine/htdocs
-md5sum -c < ../sums.md5
-# temp: | grep --invert-match ': OK'
+md5sum -c < ../sums.md5 | grep --invert-match ': OK'
