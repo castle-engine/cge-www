a4546fbdcd6981fe04aca6be496588ef575e4241
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-01-26 05:14:43 +0000

    * 3 demo screenshots to show fill mode "solid wireframe" and "silhouette"
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@2554 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/kambi_vrml_game_engine.php
+++ htdocs/kambi_vrml_game_engine.php
@@ -22,6 +22,9 @@
     '<tr><td>' . medium_image_progs_demo_core("fountain_shadows.png", '&quot;The Fountain&quot; level textured with shadows') .
     '<tr><td>' . medium_image_progs_demo_core("fountain_toon_shading.png", '&quot;The Fountain&quot; level with toon shading GLSL program') .
     '<tr><td>' . medium_image_progs_demo_core("fountain_bump_mapping_good_materials.png", '&quot;The Fountain&quot; level with bump mapping used') .
+    '<tr><td>' . medium_image_progs_demo_core("gate_fill_mode_normal.png", '&quot;The Gate&quot; level with normal fill mode') .
+    '<tr><td>' . medium_image_progs_demo_core("gate_fill_mode_solid_wireframe.png", '&quot;The Gate&quot; level with solid wireframe fill mode') .
+    '<tr><td>' . medium_image_progs_demo_core("gate_fill_mode_silhouette.png", '&quot;The Gate&quot; level with silhouette fill mode') .
     '</table>';
 ?>
 
