c6d6da04e8778b02010f6572b01ff15676fcbb74
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-09-05 06:04:16 +0000

    r3678@chantal:  michalis | 2008-09-03 22:56:55 +0200  * docs updated that HAnim is supported, also comments about humanoidBody apply to HAnim 1.1
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@3272 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrml_implementation_status.php
+++ htdocs/vrml_implementation_status.php
@@ -244,6 +244,34 @@ Shape {
     <tt>IntegerTrigger</tt>, <tt>TimeTrigger</tt></p>
 
   <li><p><tt>Rectangle2D</tt>
+
+  <li><p>HAnim nodes:
+
+    <p><tt>Humanoid</tt>, <tt>Joint</tt>, <tt>Segment</tt>,
+    <tt>Site</tt>, <tt>Displacer</tt>
+
+    <p><tt>HAnimHumanoid</tt>, <tt>HAnimJoint</tt>,
+    <tt>HAnimSegment</tt>, <tt>HAnimSite</tt>, <tt>HAnimDisplacer</tt>
+    (X3D version)
+
+    <p>As you see, X3D version has exactly the same nodes, working the same way,
+    but with <tt>HAnim</tt> prefix before node name. (I have no idea why
+    this prefix was added in X3D specification, but it's supported.)
+    Actually we allow both versions (with <tt>HAnim</tt> prefix and without)
+    in all VRML and X3D versions (<?php echo a_href_page_hashlink("with our
+    enfgine you can generally mix VRML versions",
+    'kambi_vrml_extensions',
+    'section_ext_mix_vrml_1_2'); ?>). But VRML authors/generators should
+    not overuse this, and try to conform to appropriate spec where possible.
+
+    <p>The implementation takes some care to handle all existing VRML versions:
+    <a href="http://h-anim.org/Specifications/H-Anim1.0/">HAnim 1.0</a>,
+    <a href="http://h-anim.org/Specifications/H-Anim1.1/">HAnim 1.1</a>,
+    <a href="http://h-anim.org/Specifications/H-Anim200x/ISO_IEC_FCD_19774/">HAnim 200x</a>.
+
+    <p>Animating transformations of <tt>Joint</tt> nodes and such
+    is optimized, just like for <tt>Transform</tt> node,
+    be sure to select <tt>roSeparateShapeStatesNoTransform</tt> method.
 </ul>
 
 <?php echo $toc->html_section(); ?>
@@ -286,6 +314,9 @@ used.
     view3dscene on window's caption.
 
   <li><p><tt>Switch</tt>, <tt>Group</tt>, <tt>Transform</tt>
+
+    <p>Including special optimizations for animating transformations,
+    be sure to select <tt>roSeparateShapeStatesNoTransform</tt> method.
 
   <li><p><tt>Sphere</tt>, <tt>Box</tt>, <tt>Cone</tt>, <tt>Cylinder</tt>
 
