d231ebe6c64c2c006f9cd521576c8eea0e072b56
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-01-07 20:52:27 +0000

    * improved kambi_vrml_extensions.php offline version
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@4084 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/kambi_vrml_extensions.php
+++ htdocs/kambi_vrml_extensions.php
@@ -587,7 +587,7 @@ EXTERNPROTO Text3D [
     (done by blending in OpenGL, just like partially transparent materials).
     Internally, we have a simple and very nice algorithm that detects whether texture's
     alpha channel qualifies as simple yes-no or full range, see
-    <a href="apidoc/html/Images.TImage.html#AlphaChannelType">TImage.AlphaChannelType reference</a>
+    <a href="<?php echo CURRENT_URL; ?>apidoc/html/Images.TImage.html#AlphaChannelType">TImage.AlphaChannelType reference</a>
     (default tolerance values used by VRML renderer are 5 and 0.01).
     There is also a special program in <?php echo a_href_page('engine sources',
     'kambi_vrml_game_engine'); ?> (see <tt>images/tools/detect_alpha_simple_yes_no.pasprogram</tt>
