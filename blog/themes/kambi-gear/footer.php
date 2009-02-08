9307e8170a0acb0aa9a470c321c2b24b3f785cbb
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-02-08 15:46:40 +0000

    * adjusted blog footer: my copyright, SF logo
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@4251 afe8dcc5-9094-451b-8773-5f495042a737

--- blog/themes/kambi-gear/footer.php
+++ blog/themes/kambi-gear/footer.php
@@ -1,7 +1,13 @@
 			<div class="footer">
-				<div class="paddings">
-					<div class="fl"><?php _e('Powered by', 'default'); ?> WordPress</div>
-					<div class="fr"><?php _e('Designed by', 'default'); ?> <a href="http://www.mymobiles.com">My Mobiles</a></div>
+				<div class="kambi-footer-text paddings">
+
+Copyright Michalis Kamburelis.<!-- Too long: This page is considered part of documentation of my programs, and you are free to modify and further distribute it on terms of <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License</a>.--><br/>
+
+<?php _e('Powered by', 'default'); ?> WordPress. Theme "Gear" <?php _e('designed by', 'default'); ?> <a href="http://www.mymobiles.com">My Mobiles</a>, somewhat modified.<!-- Too long: by Kambi for vrmlengine.--><br/>
+
+Hosted by
+<a href="http://sourceforge.net/projects/vrmlengine"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=200653&type=9" width="80" height="15" border="0" alt="Get Kambi VRML game engine at SourceForge.net. Fast, secure and Free Open Source software downloads" /></a>
+
 				</div>
 				<div class="clear"></div>
 			</div>
