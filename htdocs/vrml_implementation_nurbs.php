f7d8cac78cffe1698941671cded10f8730d10a5d
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-09-07 18:38:09 +0000

    * NurbsPositionInterpolator implemented, both in X3D and VRML 97 versions
    * Also dummy Set_Fraction implementations (with only TODO comments) for other nurbs interpolator nodes
    * useful utilities to assign TDynSingle/DoubleArray from other precision implemented
    + tests for them in nurbs_curve_interpolators.wrl and nurbs_curve_interpolators.x3dv
    * documented on vrml_implementation_nurbs.php
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@4817 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrml_implementation_nurbs.php
+++ htdocs/vrml_implementation_nurbs.php
@@ -7,14 +7,15 @@
 <p>Supported nodes:</p>
 
 <ul>
-  <li><p><tt>NurbsPatchSurface</tt> and <tt>NurbsCurve</tt></p></li>
+  <li><p><tt>NurbsPatchSurface</tt>, <tt>NurbsCurve</tt>, <tt>NurbsPositionInterpolator</tt></p></li>
 
   <li><p>Also basic VRML 97 NURBS nodes (defined in <i>VRML 97 Amendment 1</i>
-    specification) are implemented: <tt>NurbsSurface</tt> and <tt>NurbsCurve</tt>.</p>
+    specification) are implemented: <tt>NurbsSurface</tt>, <tt>NurbsCurve</tt>,
+    <tt>NurbsPositionInterpolator</tt>.</p>
 
     <p>VRML 97 versions are similar, but not 100% the same as their X3D counterparts.
-    For example, only X3D versions have <tt>xClosed</tt> fields.
-    Only VRML 97 versions have <tt>ccw</tt> field.
+    For example, only X3D surfaces have <tt>xClosed</tt> fields.
+    Only VRML 97 surfaces have <tt>ccw</tt> field.
     VRML 97 versions specify <tt>coord</tt> as direct <tt>MFVec3f</tt> field,
     while X3D versions specify <tt>coord</tt> as <tt>SFNode</tt> (containing
     <tt>Coordinate</tt> or similar node).</p>
