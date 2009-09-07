7ae191f1f4a4fb823f37b3082c772123b66c2ca0
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-09-07 18:39:30 +0000

    * nurbs_curve_interpolators.wrl needs "order 3" to behave the same.
    * Documented VRML <-> X3D differences better on vrml_implementation_nurbs.php
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@4822 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrml_implementation_nurbs.php
+++ htdocs/vrml_implementation_nurbs.php
@@ -13,12 +13,19 @@
     specification) are handled: <tt>NurbsSurface</tt>, <tt>NurbsCurve</tt>,
     <tt>NurbsPositionInterpolator</tt>.</p>
 
-    <p>VRML 97 versions are similar, but not 100% the same as their X3D counterparts.
-    For example, only X3D surfaces have <tt>xClosed</tt> fields.
-    Only VRML 97 surfaces have <tt>ccw</tt> field.
-    VRML 97 versions specify <tt>coord</tt> as direct <tt>MFVec3f</tt> field,
-    while X3D versions specify <tt>coord</tt> as <tt>SFNode</tt> (containing
-    <tt>Coordinate</tt> or similar node).</p>
+    <p>VRML 97 versions are similar, but not 100% the same as their X3D counterparts.</p>
+
+    <ul>
+      <li><p>Only X3D surfaces have <tt>xClosed</tt> fields. We treat TRUE value there as "possibly closed", that is &mdash; if field indicates closed, we still check if limiting points match (X3D spec suggests we should do this, as far as I understand). This means X3D models may be loaded faster &mdash; if xClosed = FALSE, we do not even check if limiting points match.</p></li>
+
+      <li><p>Only VRML 97 surfaces have <tt>ccw</tt> field.</p></li>
+
+      <li><p>VRML 97 versions specify <tt>coord</tt> as direct <tt>MFVec3f</tt> field, while X3D versions specify <tt>coord</tt> as <tt>SFNode</tt> (containing <tt>Coordinate</tt> or similar node).</p></li>
+
+      <li><p>VRML 97 <tt>NurbsPositionInterpolator</tt> has different field names (keyValue, keyWeight, following interpolator conventions) than X3D <tt>NurbsPositionInterpolator</tt> (controlPoint, weight, following nurbs conventions).</p></li>
+
+      <li><p>VRML 97 <tt>NurbsPositionInterpolator</tt> has different default value for order (4) than X3D version (3). Beware of this when converting from VRML 97 to X3D.</p></li>
+    </ul>
   </li>
 </ul>
 
