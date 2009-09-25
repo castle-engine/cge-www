c352aa2d60ef9d8840715027f1b7537b8eb14ffd
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-09-25 03:19:20 +0000

    * slerp now goes through the shortest path on unit sphere
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@4866 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrml_implementation_status.php
+++ htdocs/vrml_implementation_status.php
@@ -1089,12 +1089,12 @@ used.
     <p><tt>CoordinateInterpolator</tt>,
     <tt>CoordinateInterpolator2D</tt> (X3D), <tt>NormalInterpolator</tt></p>
 
+    <p>Interpolation of OrientationInterpolator correctly goes through
+    the shortest path on the unit sphere, with constant velocity.</p>
+
     <p><i>TODO</i>: Interpolation of ColorInterpolator simply interpolates
     3D vectors, so it interpolates in RGB space (while spec says to interpolate
     in nice HSV space).</p>
-
-    <p>Interpolation of OrientationInterpolator simply
-    interpolates 4D vectors, instead of a nice interpolation on the unit sphere.</p>
 
     <p>Interpolation of NormalInterpolator simply interpolates
     3D vectors (and normalizes afterwards), instead of
