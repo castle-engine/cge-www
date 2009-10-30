b0fb414694dacbda231d2b80a2c7e25c2aaf82a4
Michalis Kamburelis <michalis.kambi@gmail.com>
2009-10-30 04:07:57 +0000

    * show/hide mechanism in JavaScript, useful for todays news item
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@4903 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrmlengine_functions.php
+++ htdocs/vrmlengine_functions.php
@@ -40,7 +40,30 @@ function echo_header_bonus ()
 
   pre.terminal { background-color: #EEE;  padding: 0.3em; }
   tt.terminal { background-color: #EEE; }
+
+  span.js_link { text-decoration: underline; color: #00F; cursor: pointer; }
 --></style>
+
+<script type="text/javascript">
+/* Based on http://www.netlobo.com/javascript_get_element_id.html */
+function kambi_get_element_by_id(id)
+{
+  if (document.getElementById)
+    return document.getElementById(id); else
+  if (document.all)
+    return document.all[id]; else
+  if (document.layers)
+    return document.layers[id];
+}
+
+function kambi_toggle_display(id)
+{
+  var element = kambi_get_element_by_id(id);
+  if (element.style.display == "none")
+    element.style.display=""; else
+    element.style.display="none";
+}
+</script>
 
   <?php
 }
