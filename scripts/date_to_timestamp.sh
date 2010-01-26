9e7ada6023aab509a7d3d9b8eb8f25b389ec497a
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-01-26 00:19:33 +0000

    - remove pubDate set by date_to_timestamp.sh script, - remove useless date_to_timestamp.sh script
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@5332 afe8dcc5-9094-451b-8773-5f495042a737

--- scripts/date_to_timestamp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-
-# see date_timestamp in ../htdocs/changes_log_common.php
-#
-# Date formats understood are the same as for `date', but (for future)
-# limit yourself to just ISO date as yyyyy-mm-dd.
-
-date -u --date="$1 12:00" '+%s'
