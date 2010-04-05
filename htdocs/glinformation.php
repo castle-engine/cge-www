1ccbd91dac0627f23f6fa329b19afc53ac715e53
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-04-05 20:12:07 +0000

    * update glinformation.php page, more useful and less useless info :), also indicate that glut binary is packed with normal binary
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@5831 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/glinformation.php
+++ htdocs/glinformation.php
@@ -6,39 +6,27 @@
 ?>
 
 <?php echo pretty_heading("glinformation", VERSION_GLINFORMATION); ?>
-<h4>Print OpenGL capabilities</h4>
+<h4>Output OpenGL information</h4>
 
-<!-- ============================================================= -->
+<p>This tiny program asks the OpenGL library installed on your system
+about it's version and features. As OpenGL implementation is usually closely
+tied to your graphic card model and drivers, this provides information
+about your GPU, useful for developers of OpenGL (3D) programs.</p>
 
-<p>This tiny program tests that OpenGL library is installed and
-writes on standard output a lot of information about default
-OpenGL context.
+<p>Download glinformation:</p>
 
-<ul>
-  <li>If you don't know what OpenGL is then you probably don't need to
-    use this program. Unless some developer asked you to use glinformation
-    to check do you have a good OpenGL implementation installed.</li>
-
-  <li>If you know what OpenGL is then you probably realize how trivial
-    this program is :) All is done by creating a temporary
-    window with OpenGL context and asking for many values with glGet*
-    functions.</li>
-</ul>
-
-<p>Download glinformation:
 <?php echo_standard_program_download('glinformation', 'glinformation', VERSION_GLINFORMATION,
   $std_releases_post_1_8_0); ?>
 
-<p><?php echo SOURCES_OF_THIS_PROG_ARE_AVAIL; ?>
+<p><?php echo SOURCES_OF_THIS_PROG_ARE_AVAIL; ?></p>
 
 <h3>Optional command-line options</h3>
 
 <p>You can call glinformation with some of the options listed below.
 Requested OpenGL context will have given capabilities
-(in case of bit sizes, you provide the <i>minimum</i>
-requested bit size). If you know OpenGL, then the meaning
-of these options should be self-explanatory. They are useful to check
-e.g. is your graphic card able to provide 16-bit stencil buffer.
+(in case of bit sizes, you specify the <i>minimum</i>
+requested bit size). They are useful to check
+e.g. is your graphic card able to provide 16-bit stencil buffer.</p>
 
 <pre>
   -s / --stencil-bits STENCIL-BUFFER-BIT-SIZE
@@ -59,27 +47,23 @@ e.g. is your graphic card able to provide 16-bit stencil buffer.
 <?php echo a_href_page("standard options understood by my OpenGL programs",
 "opengl_options") ?> are allowed, see also <?php echo a_href_page(
 "some notes about command-line options understood by my programs",
-"common_options") ?>.
+"common_options") ?>.</p>
 
 <h3>GLUT versions</h3>
 
-I don't use GLUT in my programs. But, just in case (since
-glinformation is intended to be a diagnostical program, that may be used
+<p>I don't use <a href="http://www.opengl.org/resources/libraries/glut/">GLUT</a>
+in my programs. But, just in case (since
+glinformation is intended to be a diagnostic tool, that may be used
 in case there is some error somewhere &mdash; maybe in my own code),
 I provide also a special GLUT version of glinformation. This initializes
 OpenGL context using GLUT, then writes the same info as normal
-version.
-
-<?php echo_standard_program_download('glinformation_glut', 'glinformation_glut',
-  VERSION_GLINFORMATION_GLUT, $std_releases_post_1_8_0); ?>
-
-<p><?php echo SOURCES_OF_THIS_PROG_ARE_AVAIL; ?>
+version. The <tt>glinformation_glut</tt> binary is inside the above download.</p>
 
 <h3>glxinfo notes</h3>
 
 <p>XWindows users can also use <tt>glxinfo</tt> program.
 It's purpose is similar to glinformation (but it's only for GLX,
-so it's able to write some info like all possible visual configurations).
+so it's able to write some info like all possible visual configurations).</p>
 
 <?php
 /* Too short to mention, but actually:
