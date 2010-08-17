55e9aeab7a7e10674860a86dec68d5a4ec74d7bd
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-08-17 11:19:16 +0000

    Document VRML 1.0 and multiple materials limitations now.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@6448 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrml_implementation_shape.php
+++ htdocs/vrml_implementation_shape.php
@@ -8,6 +8,25 @@
 
 <ul>
   <li><p><tt>Shape</tt>, <tt>Appearance</tt>, <tt>Material</tt></p></li>
+
+  <li><b>VRML 1.0 and multiple materials</b>: multiple materials
+    within a single VRML 1.0 <tt>Material</tt> node work 100%
+    correctly if you change only emissive and transparency,
+    or only diffuse and transparency for each index.
+    For complicated cases (like when you change diffuse, and specular,
+    and emissive...) for each material index -&gt; they will fail.</p>
+
+    <p>This is a wontfix. For OpenGL fixed-function pipeline,
+    changing all <tt>glMaterial</tt> settings too often (like for
+    a vertex or a face) is prohibitively slow.
+    It's also terribly memory consuming (for
+    <?php echo a_href_page("castle", "castle") ?>, display lists of animations
+    of spider and spider queen were eating 130 MB with naive implementation,
+    vs 10 MB with current implementation).</p>
+
+    <p>VRML 2.0 and X3D removed this idea, replacing it with much
+    saner <tt>Color</tt> and <tt>ColorRGBA</tt> nodes, that are implemented
+    fully.</p>
 </ul>
 
 <p><i>TODO</i>: FillProperties, LineProperties, TwoSidedMaterial are missing.</p>
