4a04f54d6c2ea1c70781232bc266c6913e1e1125
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-10-03 03:35:05 +0000

    sidebar to the right.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@6784 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrml_implementation_common.php
+++ htdocs/vrml_implementation_common.php
@@ -10,15 +10,15 @@ function vrmlx3d_header($a_page_title)
 {
   common_header($a_page_title, LANG_EN);
   echo '<table class="layout">
-    <col class="sidebar" />
     <col class="content" />
+    <col class="sidebar" />
     <tr><td class="layout">';
-  vrmlx3d_sidebar();
-  echo '</td><td class="layout">';
 }
 
 function vrmlx3d_footer()
 {
+  echo '</td><td class="layout">';
+  vrmlx3d_sidebar();
   echo '</td></tr></table>';
   common_footer();
 }
