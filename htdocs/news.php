290b90fafd1f84d7029522c353589298a5bfbbe9
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-10-09 00:04:09 +0000

    Show Previous/Next links at news items.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@6870 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/news.php
+++ htdocs/news.php
@@ -22,14 +22,40 @@ function vrmlengine_sitemap_add_news()
   }
 }
 
-/* Return news item from $id, or NULL if not found. */
-function vrmlengine_news_item_by_id($id)
+/* Return news item from $id. Finds also previous and next news items
+   (NULL if current item is first or last). Returns all three items
+   as NULL if not found.
+
+   So to easily detect if the item was found, it's enough to just
+   check if returned $current === NULL.  */
+function vrmlengine_news_item_by_id($id, &$previous, &$current, &$next)
 {
   global $news;
+
+  $previous = NULL;
+  $current = NULL;
+  $next = NULL;
+
   foreach($news as $news_item)
-    if ($news_item['id'] == $id)
-      return $news_item;
-  return NULL;
+  {
+    $previous = $current;
+    $current = $next;
+    $next = $news_item;
+
+    if ($current !== NULL && $current['id'] == $id)
+      return;
+  }
+
+  /* one more step */
+  $previous = $current;
+  $current = $next;
+  $next = NULL;
+  if ($current !== NULL && $current['id'] == $id)
+    return;
+
+  $previous = NULL;
+  $current = NULL;
+  $next = NULL;
 }
 
 /* End of functions ---------------------------------------------------------- */
@@ -45,18 +71,31 @@ function vrmlengine_news_item_by_id($id)
 
   if (isset($_GET['item']))
   {
-    $item = vrmlengine_news_item_by_id($_GET['item']);
+    vrmlengine_news_item_by_id($_GET['item'], $previous_item, $item, $next_item);
     if ($item === NULL)
       die('Invalid news item "' . $_GET['item'] . '"');
   } else
+  {
     /* By default, if someone uses just "news.php" URL,
        choose the latest news item. */
+    $previous_item = NULL;
     $item = $news[0];
+    if (count($news) > 1)
+      $next_item = $news[1]; else
+      $next_item = NULL;
+  }
 
   vrmlengine_header($item['title'] . ' | News', NULL,
     array('index', 'news', 'news.php?item=' . $item['id']));
 
-  echo '<div class="news_item">' . news_to_html($item) . '</div>';
+  /* Remember that naming "previous / next" in the array
+     is reversed to what user considers previous/next (earlier/later) */
+  if ($next_item)
+    echo '<div class="news_previous_next" style="float: left"><a title="' . $next_item['title'] . '" href="news.php?item=' . $next_item['id'] . '">&laquo; Previous</a></div>';
+  if ($previous_item)
+    echo '<div class="news_previous_next" style="float: right"><a title="' . $previous_item['title'] . '" href="news.php?item=' . $previous_item['id'] . '">Next &raquo;</a></div>';
+
+  echo '<div class="news_item" style="clear: both">' . news_to_html($item) . '</div>';
 
   vrmlengine_footer();
 ?>
