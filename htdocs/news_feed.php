cdcd48ca1a7fff49384eeb195270ce610c7ffc9f
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-10-09 00:01:28 +0000

    Limit the RSS feed to 10 items, to not be too long.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@6860 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/news_feed.php
+++ htdocs/news_feed.php
@@ -7,6 +7,10 @@
   require_once 'kambi-php-lib/class_rss_generator.inc.php';
   require_once 'news_common.php';
 
+  define('FEED_MAX_COUNT', 10);
+  if (count($news) > FEED_MAX_COUNT)
+    $news = array_slice($news, 0, FEED_MAX_COUNT);
+
   $rss = new rss_generator('Kambi VRML game engine - changes log');
   $rss->link = 'http://vrmlengine.sourceforge.net/';
   $rss->description = 'Kambi VRML game engine - all changes, releases, etc.';
