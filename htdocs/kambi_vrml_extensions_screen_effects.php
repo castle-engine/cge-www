0c0a5cfa0aada0eab2ccd2efa007d8ab81b207e5
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-11-16 23:14:08 +0000

    Finally, the documentation for ScreenEffect is written.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@7271 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/kambi_vrml_extensions_screen_effects.php
+++ htdocs/kambi_vrml_extensions_screen_effects.php
@@ -5,8 +5,14 @@
   vrmlengine_header('Screen Effects', NULL,
     array('vrml_x3d', 'kambi_vrml_extensions', 'kambi_vrml_extensions_screen_effects'));
 
-// $toc = new TableOfContents(array());
-// $toc->echo_numbers = true;
+$toc = new TableOfContents(array(
+  new TocItem('Intro', 'intro'),
+  new TocItem('Definition', 'definition'),
+  new TocItem('Examples', 'examples'),
+  new TocItem('Details', 'details'),
+  new TocItem('Todos', 'todos'),
+));
+$toc->echo_numbers = true;
 
   echo vrmlengine_thumbs(array(
     array('filename' => 'screen_effect_trees.png', 'titlealt' => 'Another screen effect example'),
@@ -18,14 +24,14 @@
   ));
 
   echo pretty_heading($page_title);
-
-  // <!--p>Contents:
-  // < ?php echo $toc->html_toc(); ? >
-  // < ?php echo $toc->html_section(); ? -->
 ?>
 
-<p><i>Screen effects</i> allow you to create many graphic effects
-by post-processing the rendered image. For demos, see:</p>
+<p>Contents:</p>
+<?php echo $toc->html_toc(); ?>
+<?php echo $toc->html_section(); ?>
+
+<p><i>Screen effects</i> allow you to create many effects
+by processing the rendered image. For&nbsp;demos, see:</p>
 
 <ul>
   <li><p><?php echo a_href_page('view3dscene', 'view3dscene') ?>
@@ -39,6 +45,166 @@ by post-processing the rendered image. For demos, see:</p>
     demo with an example castle model underneath.</p></li>
 </ul>
 
-<?php
-  vrmlengine_footer();
+<?php echo $toc->html_section(); ?>
+
+<p>Define your own screen effects in VRML/X3D files by using
+the <tt>ScreenEffect</tt> node:</p>
+
+<?php echo node_begin("ScreenEffect : X3DChildNode");
+  $node_format_fd_name_pad = 20;
+  $node_format_fd_def_pad = 15;
+  echo
+  node_field('SFNode', '[in,out]', 'metadata', 'NULL', '[X3DMetadataObject]') .
+  node_field('SFBool', '[in,out]', 'enabled', 'TRUE') .
+  node_field('SFBool', '[in,out]', 'needsDepth', 'FALSE') .
+  node_field('MFNode', '[in,out]', 'shaders', '[]', '[X3DShaderNode]') .
+  node_end();
 ?>
+
+<p>A <tt>ScreenEffect</tt> is active if it's a part of normal VRML/X3D
+transformation hierarchy (in normal words: it's not inside a disabled
+child of the <tt>Switch</tt> node or such) and when the <tt>"enabled"</tt>
+field is <tt>TRUE</tt>.
+In the simple cases, you usually just add <tt>ScreenEffect</tt> node
+anywhere at the top level of your VRML/X3D file. If you use many
+<tt>ScreenEffect</tt> nodes, then their order matters: 
+they process the rendered screen in the given order.</p>
+
+<p>You have to specify a shader to process the rendered screen by the
+<tt>"shaders"</tt> field. This works exactly like the standard X3D
+<tt>"Appearance.shaders"</tt>, by selecting a first supported shader.
+Right now our engine supports only GLSL (OpenGL shading language) shaders
+inside <tt>ComposedShader</tt> nodes, see <?php echo a_href_page(
+'the general overview of shaders support in our engine', 'vrml_implementation_shaders'); ?>
+ and <a href="http://www.web3d.org/x3d/specifications/ISO-IEC-19775-1.2-X3D-AbstractSpecification/Part01/components/shaders.html">X3D
+"Programmable shaders component" specification</a> and of course
+the <a href="http://www.opengl.org/documentation/glsl/">GLSL documentation</a>.</p>
+
+<p>The shader inside <tt>ScreenEffect</tt> receives some special uniform
+variables. The most important is the <tt>"screen"</tt>, which is a texture
+rectangle (<tt>ARB_texture_rectangle</tt> in OpenGL terms)
+containing the screen colors. Your fragment shader is supposed to calculate
+<tt>gl_FragColor</tt> for a pixel using this <tt>"screen"</tt>.</p>
+
+<?php echo $toc->html_section(); ?>
+
+<p>A simplest example:</p>
+
+<pre class="vrml_code">
+ScreenEffect {
+  shaders ComposedShader {
+    language "GLSL"
+    parts ShaderPart { type "FRAGMENT" url "
+#extension GL_ARB_texture_rectangle : enable
+uniform sampler2DRect screen;
+void main (void)
+{
+  gl_FragColor = texture2DRect(screen, gl_TexCoord[0].st);
+}
+" } } }
+</pre>
+
+<p>The above example processes the screen without making any changes.
+You now have the full power of GLSL to modify it to make any changes
+to colors, sampled positions and such. For example
+make colors two times smaller (darker) by just dividing by 2.0:</p>
+
+<pre class="vrml_code">
+#extension GL_ARB_texture_rectangle : enable
+uniform sampler2DRect screen;
+void main (void)
+{
+  gl_FragColor = texture2DRect(screen, gl_TexCoord[0].st) / 2.0;
+}
+</pre>
+
+<p>Or turn the screen upside-down by changing the 2nd coordinate
+<tt>gl_TexCoord[0].t</tt>:</p>
+
+<pre class="vrml_code">
+#extension GL_ARB_texture_rectangle : enable
+uniform sampler2DRect screen;
+uniform int screen_height;
+void main (void)
+{
+  gl_FragColor = texture2DRect(screen,
+    vec2(gl_TexCoord[0].s, float(screen_height) - gl_TexCoord[0].t));
+}
+</pre>
+
+<?php echo $toc->html_section(); ?>
+
+<p>Details about special uniform variables passed to the <tt>ScreenEffect</tt>
+shader:</p>
+
+<ul>
+  <li><p>The uniform variable named <tt>"screen"</tt> contains
+    the current screen contents. It is
+    a <a href="http://www.opengl.org/registry/specs/ARB/texture_rectangle.txt">texture rectangle</a>,
+    which means it should be declared in GLSL as <tt>sampler2DRect</tt>
+    and may be sampled e.g. by <tt>texture2DRect</tt>.</p>
+
+    <p>Note that the texture rectangle coordinates are in range
+    <tt>[0..width, 0..height]</tt> (unlike normal OpenGL textures
+    that have coordinates in range <tt>[0..1, 0..1]</tt>).
+    This is usually comfortable when writing screen effects shaders,
+    for example you know that <tt>(gl_TexCoord[0].s - 1.0)</tt> is
+    "one pixel to the left".</p>
+
+    <p>You can of course sample <tt>"screen"</tt> however you like.
+    The <tt>gl_TexCoord[0].st</tt> is the position that would
+    normally be used for given pixel &mdash; you can use it,
+    or calculate some other position depending on it,
+    or even totally ignore it for special tricks.
+    Note that using <tt>gl_FragCoord.st</tt> as texture coordinate
+    will work in simple cases too,
+    but it's not adviced, because it will not work intuitively
+    when you use <a href="http://vrmlengine.sourceforge.net/vrml_engine_doc/output/xsl/html/ch02s04.html">custom viewports</a>
+    with our engine. <tt>gl_TexCoord[0].st</tt>
+    will cooperate nicely with custom viewports.</p></li>
+
+  <li><p>We also pass <tt>"screen_width"</tt>, <tt>"screen_height"</tt>
+    integers to the shader. These give you the size of the screen.
+    (On new GPUs, you could get them with GLSL function <tt>textureSize</tt>,
+    but it's not available on older GPUs/OpenGL versions.)</p>
+
+  <li><p>If you set <tt>"needsDepth"</tt> to <tt>TRUE</tt> then we also pass
+    <tt>"screen_depth"</tt>, a rectangle texture containing depth buffer contents,
+    to the shader. You can define a uniform variable (<tt>sampler2DRect</tt> or
+    <tt>sampler2DRectShadow</tt>, the former is usually more useful for scren effects)
+    to handle this texture in your shader.
+    Remember to <b>always set "needsDepth" to TRUE if you want to use
+    the <tt>"screen_depth"</tt></b>.</p>
+
+    <p>You can query depth information at any pixel for various effects.
+    Remember that you are not limited to querying the depth of the current
+    pixel, you can also query the pixels around (for example,
+    for <a href="http://en.wikipedia.org/wiki/Screen_Space_Ambient_Occlusion">Screen Space Ambient Occlusion</a>).
+    The "Flashlight" effect in
+    <?php echo a_href_page('view3dscene', 'view3dscene') ?>
+    queries a couple of pixels in the middle of the screen to estimate
+    the distance to an object in front of the camera, which in turn determines
+    the flashlight circle size.</p></li>
+
+  <li><p>Remember that you can pass other uniform values to the shader,
+    just like with any other <tt>ComposedShader</tt> nodes.
+    For example you can pass an additional helper texture
+    (e.g. a headlight mask) to the shader. Or you can route the current
+    time (from <tt>TimeSensor</tt>) to the shader, to make your effect
+    based on time.</p></li>
+</ul>
+
+<?php echo $toc->html_section(); ?>
+
+<p><tt>ScreenEffect</tt> under a dynamic <tt>Switch</tt> doesn't
+react properly &mdash; changing <tt>"Switch.whichChoice"</tt> doesn't
+deactivate the old effect, and doesn't activate the new effect.
+<!-- We would have to move them to Shapes tree for this,
+which would turn all our processing of ScreenEffectNodes
+into iterating over shapes, which may be too slow...  -->
+For now, do not place <tt>ScreenEffect</tt> under <tt>Switch</tt>
+that can change during the world life. If you want to (de)activate
+the shader dynamically (based on some events in your world),
+you can send events to the exposed <tt>"enabled"</tt> field.</p>
+
+<?php vrmlengine_footer(); ?>
