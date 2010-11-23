0fb98430a47ed73d6fad4bf5df724c1f3e458624
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-11-23 22:55:36 +0000

    Nice "help wanted" text for developers wanting to make more native Mac OS X port.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@7356 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/macosx_requirements.php
+++ htdocs/macosx_requirements.php
@@ -1,9 +1,22 @@
 <?php
   require_once "vrmlengine_functions.php";
   vrmlengine_header("Dependencies of my programs on Mac OS X", NULL, array('other'));
+
+  $toc = new TableOfContents(
+    array(
+      new TocItem('What do you need to use my programs on Mac OS X now', 'requirements_now'),
+      new TocItem('Developers: help wanted for making more native Mac OS X port', 'help_wanted'),
+    )
+  );
+  $toc->echo_numbers = true;
+
+  echo pretty_heading($page_title);
 ?>
 
-<h1>Dependencies of my programs on Mac OS X</h1>
+<p>Contents:
+<?php echo $toc->html_toc(); ?>
+
+<?php echo $toc->html_section(); ?>
 
 <p>Library requirements of using my engine on Mac OS X:
 
@@ -59,6 +72,61 @@ path by command like
 </pre>
 before executing programs. (you can add this to your <tt>.bashrc</tt>
 or similar file for comfort).
+
+<?php echo $toc->html_section(); ?>
+
+<p>The current way my engine and programs (view3dscene, castle etc.)
+work on Mac OS X is admittedly a simple and straightforward port from Linux.
+It doesn't look natively (like other normal Mac OS X programs),
+it requires some uncommon libraries (not only X11, but also GTK and GtkGLExt
+from fink) and so on. If you're a developer familiar with Mac OS X
+native toolkit (as far as I know, this means
+<a href="http://wiki.freepascal.org/Carbon_Interface">Carbon</a>
+or <a href="http://wiki.freepascal.org/Cocoa_Interface">Cocoa</a>)
+and <a href="http://www.freepascal.org/">FreePascal</a> &mdash; you're most
+welcome to help.</p>
+
+<p>The plan:</p>
+
+<ol>
+  <li><p><i>Port our <tt>GLWindow</tt> unit to native Mac OS X toolkit</i>.
+    This is the main work, it will make dependencies on X11, GTK, GtkGLExt
+    disappear and will provide a native look.</p>
+
+    <p>It's a matter of creating and implementing a file like
+    <tt>kambi_vrml_game_engine/src/glwindow/glwindow_(cocoa|carbon).inc</tt>,
+    based on
+    <tt>kambi_vrml_game_engine/src/glwindow/glwindow_implementation_template.inc</tt>.
+    See <?php echo a_href_page('engine sources', 'kambi_vrml_game_engine'); ?>.
+    See at other "backends" (currently GTK, WinAPI, Xlib, Glut are available)
+    for examples how to implement such thing, everything is inside
+    <tt>src/glwindow/</tt> dir.</p>
+
+    <p>Alternatively, send me a simple and clear example of FPC program
+    using Carbon/Cocoa that 1. creates and shows a window
+    2. with menu bar 3. and with OpenGL context area covering the window.
+    I should be able to port such example to my "GLWindow" then.</p>
+
+    <p>See e.g. <a href="http://wiki.freepascal.org/OS_X_Programming_Tips">FPC "OS_X Programming Tips"</a>
+    for pointers</p></li>
+
+  <li><p><i>Make "Mac OS X bundle"</i> (it's basically a directory pretending
+    to be an application). You can find view3dscene SVG icon in the sources.</p></li>
+
+  <li><p>Optionally, <i>add libpng and vorbisfile libraries to the bundle</i>.
+    We will still be able to link to them dynamically, as far as I know.
+    libpng is required for PNG reading (unless new Mac OS X already includes it?).
+    vorbisfile is required for OggVorbis playing (coming soon in view3dscene
+    3.8.0, already available in castle).</p></li>
+
+  <li><p>Finally, prepare a script to pack a nice .dmg (instead of current
+    .tar.gz) distribution (of view3dscene, or castle &mdash; doesn't matter,
+    I'll adjust it to be more general).</p></li>
+</ol>
+
+<p>Fame, fortune, and eternal gratitude from Michalis and all Mac OS X
+users of our programs will be your reward :) At such moments,
+I wish I had a kingdom and three daughters.</p>
 
 <?php
   vrmlengine_footer();
