69737b93c6091dbfd2a8fa1480414dab3963b16b
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-01-15 18:08:18 +0000

    Implement and document FogCoordinate handling.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@7778 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrml_implementation_environmentaleffects.php
+++ htdocs/vrml_implementation_environmentaleffects.php
@@ -12,9 +12,21 @@
 <ul>
   <li><p><?php echo x3d_node_link('Background'); ?>,
     <?php echo x3d_node_link('Fog'); ?></p></li>
+
+  <li><p><?php echo x3d_node_link('FogCoordinate'); ?> is also supported.
+
+    <p>Note that depths (in <tt>FogCoordinate.depth</tt>) are sensible
+    only in the [0, 1] range (like allowed by the specification),
+    and are not affected by the <tt>Fog.visibilityRange</tt>
+    (also following the spec). Effectively, this means that <tt>FogCoordinate.depth</tt>
+    does <i>not</i> substitute the mere distance to the eye in the fog equation.
+    Instead, it specifies directly the fog intensity.
+    I understand that this is the intention of the spec, although the wording
+    is clumsy (not saying directly what "implicit depth" is &mdash;
+    fog input distance or fog output intensity).</p>
 </ul>
 
-<p><i>TODO</i>: FogCoordinate, LocalFog not supported. TextureBackground not supported (waits on Background rendering rewrite &mdash; our <tt>Background</tt> rendering is quite specialized to VRML 97 background case, and it doesn't use our texture cache. This prevents from adapting it easily to use normal texture nodes.).</p>
+<p><i>TODO</i>: LocalFog not supported. TextureBackground not supported (waits on Background rendering rewrite &mdash; our <tt>Background</tt> rendering is quite specialized to VRML 97 background case, and it doesn't use our texture cache. This prevents from adapting it easily to use normal texture nodes.).</p>
 
 <?php
   x3d_status_footer();
