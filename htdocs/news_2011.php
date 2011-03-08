c2bd6976905bad0af54e065bf6e196dbe81ae3ed
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-03-08 21:56:18 +0000

    Next news: add screenshots, and notes to nightly builds.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@8582 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/news_2011.php
+++ htdocs/news_2011.php
@@ -7,7 +7,23 @@ array_push($news,
           'day' => 8,
           'short_description' => '',
           'description' =>
-'<ol>
+vrmlengine_thumbs(array(
+  array('filename' => 'metallic_shiny.png', 'titlealt' => 'Shiny dark metallic material under multiple lights, with per-pixel lighting.'),
+  array('filename' => 'castle_light_2.png', 'titlealt' => 'Castle fountain level with shaders, fun with lighting 2'),
+  array('filename' => 'castle_light_1.png', 'titlealt' => 'Castle fountain level with shaders, fun with lighting 1'),
+  array('filename' => 'castle_light_3.png', 'titlealt' => 'Castle fountain level with shaders, fun with lighting 3'),
+  array('filename' => 'shadow_map_spot.png', 'titlealt' => 'Spot light casting shadows'),
+  array('filename' => 'volumetric_animated_fog_all.png', 'titlealt' => 'Volumetric fog'),
+  array('filename' => 'volumetric_animated_fog_no_fog.png', 'titlealt' => 'Scene for the volumetric fog, here visible with fog turned off'),
+  array('filename' => 'volumetric_animated_fog_no_light.png', 'titlealt' => 'Volumetric fog, with normal lighting turned off'),
+  array('filename' => 'fancy_light_spot_shape.png', 'titlealt' => 'Textured spot light with shadow'),
+  array('filename' => 'flowers.png', 'titlealt' => 'Flowers bending under the wind, transformed on GPU in object space'),
+  array('filename' => 'fresnel_and_toon.png', 'titlealt' => 'Toon and Fresnel effects combined'),
+  array('filename' => 'noise.png', 'titlealt' => '3D and 2D smooth noise on GPU, wrapped in ShaderTexture'),
+)) .
+'<p>A lot of great news about engine development below :) As always, remember you can try them all immediately by downloading a binary from our <a href="http://michalis.ii.uni.wroc.pl/vrmlengine-snapshots/">nightly builds</a>!</p>
+
+<ol>
   <li><p>We have a new shiny method of rendering everything through shaders (OpenGL Shading Language, aka GLSL). All of the standard X3D effects, and our extensions, are implemented in this mode. This results in much better look of many scenes.</p>
 
     <p>By default, the shader rendering is used only for shapes that require it &mdash; are shadow map receivers, use bump mapping, or have an explicit shader source code assigned (by <a href="http://vrmlengine.sourceforge.net/vrml_implementation_shaders.php">the ComposedShader node</a>, or by the new <a href="http://vrmlengine.sourceforge.net/compositing_shaders.php">Compositing Shaders extensions</a>.). For testing purposes, you can switch to <i>"View -&gt; Shaders -&gt; Enable For Everything"</i> in <a href="http://vrmlengine.sourceforge.net/view3dscene.php">view3dscene</a> menu. The results should be the same (or better) than current renderer.
