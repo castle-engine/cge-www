c5d67aa44824f2496c3169009deb7bb6cf716c1b
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-03-29 22:33:36 +0000

    Document LineProperties implementation.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@8667 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrml_implementation_shape.php
+++ htdocs/vrml_implementation_shape.php
@@ -25,15 +25,24 @@
     a vertex or a face) is prohibitively slow.
     It's also terribly memory consuming (for
     <?php echo a_href_page("castle", "castle") ?>, display lists of animations
+    (in the older days when we used display lists)
     of spider and spider queen were eating 130 MB with naive implementation,
     vs 10 MB with current implementation).</p>
 
     <p>VRML 2.0 and X3D removed this idea, replacing it with much
     saner <tt>Color</tt> and <tt>ColorRGBA</tt> nodes, that are implemented
     fully.</p>
+
+  <li><p><?php echo x3d_node_link('LineProperties'); ?>.
+    Implemented, allows to change line width and type (pattern).
+    Suitable for all line geometry,
+    in particular <tt>IndexedLineSet</tt> and <tt>LineSet</tt>.
+    It also affects normal (filled) geometry (like <tt>IndexedFaceSet</tt>)
+    when viewed in wireframe mode (see view3dscene "View -&gt; Fill Mode"
+    menu).</p>
 </ul>
 
-<p><i>TODO</i>: FillProperties, LineProperties, TwoSidedMaterial are missing.</p>
+<p><i>TODO</i>: FillProperties, TwoSidedMaterial are missing.</p>
 
 <?php
   x3d_status_footer();
