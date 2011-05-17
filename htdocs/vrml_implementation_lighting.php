b2843727397152c1cc3b1feb46e12df7146250e0
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-05-17 03:56:15 +0000

    Per-pixel lighting notes.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@8931 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrml_implementation_lighting.php
+++ htdocs/vrml_implementation_lighting.php
@@ -2,7 +2,19 @@
   require_once 'vrml_implementation_common.php';
   x3d_status_header('Lighting', 'lighting',
     'This component defines light source nodes.');
+
+  $toc = new TableOfContents(
+    array(
+      new TocItem('Support', 'support'),
+      new TocItem('Per-pixel lighting', 'per_pixel_lighting'),
+    ));
+  $toc->echo_numbers = true;
 ?>
+
+<p>Contents:
+<?php echo $toc->html_toc(); ?>
+
+<?php echo $toc->html_section(); ?>
 
 <p>Supported nodes:</p>
 
@@ -28,6 +40,43 @@
     value of this field is consistent with VRML 2.0 specification
     (that always wants directional lights non-global, and other lights
     always global).
+</ul>
+
+<?php echo $toc->html_section(); ?>
+
+<p>By default we render most shapes using OpenGL fixed-function
+pipeline, and VRML/X3D lights are simply used as OpenGL lights.
+This means that lighting is calculated per-vertex (Gouraud shading).
+This is fast, but not always perfect.</p>
+
+<p>Using the <i>"Shaders-&gt;Enable For Everything"</i> option in view3dscene
+forces everything to be rendered through GLSL shaders, which always
+perform per-pixel lighting.
+This is usually slower but also more beautiful than default
+<i>"Shaders-&gt;Enable For Required"</i>.</p>
+
+<ul>
+  <li><p>Per-pixel lighting means that local lighting effects,
+    in particular spot light cones and specular highlights,
+    are precisely rendered.</p></li>
+  <li><p>Lights attenuation is also calculated per-pixel,
+    this sometimes gives much better results.</li>
+  <li>
+    <?php
+      echo vrmlengine_thumbs(array(
+        array('filename' => 'light_street_lights_radius_no_shaders.png', 'titlealt' => 'Lights with radius with fixed-function pipeline rendering'),
+        array('filename' => 'light_street_lights_radius_shaders.png', 'titlealt' => 'Light with radius with per-pixel lighting (shader pipeline)'),
+      ));
+    ?>
+    <p>Light radius is also checked per-pixel when necessary
+    (when shape is partially inside, partially outside the light radius).
+    <!-- (previously, only per-shape). -->
+    This allows to use <tt>"radius"</tt> field (on point and spot lights)
+    for much more dramatic lighting effects.
+    For example, compare the two screenshots from
+    light_street_lights_radius demo on the right (from
+    <?php echo a_href_page('our VRML/X3D demo models', 'demo_models'); ?>).
+    </p></li>
 </ul>
 
 <?php
