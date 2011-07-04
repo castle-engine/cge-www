d03b8d8680abfb43cc1d6539cdc5b8d1776d5d48
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-07-04 09:56:04 +0000

    No such thing as "Separate Shapes No Transform" option anymore.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@9412 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/vrml_implementation_grouping.php
+++ htdocs/vrml_implementation_grouping.php
@@ -21,9 +21,7 @@
          <?php echo x3d_node_link('Group'); ?>,
          <?php echo x3d_node_link('Transform'); ?></p>
 
-    <p>Including special optimizations for animating transformations
-    (be sure to select <i>Separate Shapes No Transform</i> optimization method,
-    default since view3dscene &gt;= 3.7.0).</p></li>
+    <p>Including special optimizations for animating transformations.</p></li>
 </ul>
 
 <p>Note that explicit bounding boxes are (for now! This may change in the future!) not used by the engine. That is, don't bother calculating / filling the fields <tt>bboxCenter</tt> and <tt>bboxSize</tt> for our engine. The engine currently always internally calculates and keeps up-to-date best-fit boxes (and spheres) for collision, all by itself.</p>
