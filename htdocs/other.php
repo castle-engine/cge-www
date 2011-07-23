2b9f9774d4532926818ed0b8a7f02a3da11b8354
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-07-23 15:10:18 +0000

    Remove "Programmers may be interested in my notes about". Not really useful.
    Moved to http://michalis.ii.uni.wroc.pl/~michalis/
    
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@9586 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/other.php
+++ htdocs/other.php
@@ -14,22 +14,6 @@ the programs developed with it:</p>
     (<b>Please</b> contribute to it, and then we'll make the wiki link more visible!)
   <li><?php echo a_href_page(
     "Blender VRML/X3D exporters", "blender_stuff"); ?></li>
-
-  <li>Programmers may be interested in my notes about:
-    <ul>
-      <li><a href="http://vrmlengine.svn.sourceforge.net/viewvc/*checkout*/vrmlengine/trunk/kambi_vrml_game_engine/examples/vrml/bump_mapping/README">Bump
-        mapping techniques</a>,</li>
-      <li><a href="http://vrmlengine.svn.sourceforge.net/viewvc/*checkout*/vrmlengine/trunk/kambi_vrml_game_engine/examples/glwindow/shading_langs/README">Shading
-        languages (some info about ARB vertex/fragment assembly programs,
-        and a lot of GLSL)</a>,</li>
-      <li><a href="https://vrmlengine.svn.sourceforge.net/svnroot/vrmlengine/trunk/kambi_vrml_game_engine/examples/vrml/terrain/TERRAIN_GENERATION_NOTES.txt">Basic terrain (noise) generation.</a>
-    </ul>
-    I wrote these documents when I was learning these techniques myself,
-    they contain some summary and my findings, and links to other useful resources.
-    You can take a look into our engine sources (in <tt>kambi_vrml_game_engine/examples/..</tt>
-    directories, just look at the URLs of these documents) to see practical
-    implementation of all these things.
-  </li>
 </ul>
 
 <p><a href="http://michalis.ii.uni.wroc.pl/~michalis/vrmlengine-snapshots/">Nightly
