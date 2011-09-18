393d6e9852135a9ca9d5b3d331c81f758bbb4012
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-09-18 09:30:46 +0000

    Add test_out_mail.php, maybe will be useful some day.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@10049 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ tests/test_out_mail.php
@@ -0,0 +1,50 @@
+<?php
+
+  /* Testing mail sending, following example from
+     https://sourceforge.net/apps/trac/sourceforge/wiki/Project%20Web%20Email%20Configuration
+     I want to make sure that I have working config, I need email working for my local piwik
+     (as piwik hosted by SF is broken), and probably wordpress in the future.
+
+     Needed local install of pear package Mail.php
+
+  // pear config-set php_dir /home/project-web/vrmlengine/pear
+  // pear config-set data_dir /home/project-web/vrmlengine/pear
+  // pear config-set test_dir /home/project-web/vrmlengine/pear/test
+  // pear config-set doc_dir /home/project-web/vrmlengine/pear/doc
+  // pear install --alldeps mail
+  */
+
+set_include_path('/home/project-web/vrmlengine/pear' . PATH_SEPARATOR
+                 . get_include_path());
+
+// testing pear
+//echo get_include_path();
+//require_once 'System.php';
+//var_dump(class_exists('System', false));
+
+include('Mail.php');
+
+$recipients = array( 'michalis.kambi@gmail.com' ); # Can be one or more emails
+
+$headers = array (
+		  'From' => 'admin@vrmlengine.sourceforge.net',
+		  'To' => join(', ', $recipients),
+		  'Subject' => 'Testing email from project web',
+		  );
+
+$body = "This was sent via php from project web!\n";
+
+$mail_object =& Mail::factory('smtp',
+			      array(
+				    'host' => 'prwebmail',
+				    'auth' => true,
+				    'username' => 'vrmlengine',
+				    'password' => 'FILL-THIS', # As set on your project's config page
+        'debug' => true, # uncomment to enable debugging
+    ));
+
+echo 'Start...' . "\n";
+
+$mail_object->send($recipients, $headers, $body);
+
+echo 'Done...' . "\n";
