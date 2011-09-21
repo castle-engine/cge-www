0eabd4bdef38ae860f9ca871bcf194650e3d442e
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-09-21 22:14:13 +0000

    Allura doesn't support donations, so just set up direct PayPal button.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@10083 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/donate.php
+++ htdocs/donate.php
@@ -21,9 +21,9 @@ and view3dscene, and our games :) Thanks!</p>
   <td>
   <p><?php flattr_button(false); ?></p>
 
-  <p>You can donate through <a href="http://flattr.com/">Flattr</a>
-  by clicking the button above. Click the button again to subscribe
-  for a longer time.</p>
+  <p>You can donate through <a href="http://flattr.com/">Flattr</a>.
+  Just click on a button above, everything will be explained.
+  Click the button again to subscribe for a longer time.</p>
   </td>
 
   <td>
@@ -39,6 +39,22 @@ and view3dscene, and our games :) Thanks!</p>
   You can propose your own feature, or agree to a feature
   proposed by another user or developer.</p>
   </td>
+
+  <td>
+  <p>
+  <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAQ22RQb9dX0B1r5MlaJbDIhzZFVKP0vUYivOeODEyk1YfQGaYot7Ya+A+60EqDSLliGSHtdm7vsa29DH64pRYLL6ibAP3KSnNIITefcfbXT+oRbAnKid0Qnigg1ece9EVCVlgaIluSjGAXT9r37UPLbcvwviq7epezBI8oaFaEkjELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIuN4M87UOMUSAgZi+oDRP5W1YeMLKINMo8GbZIGp/F8hyfv3ycCjVQ254kk4zY4UFRVkZJSxNgtZNsvANXHjeDQXeV1oXYcIjyaMOmCG4fITdx9FHe0+luu/cX04yowiKzkawTrmJkX9Yk9B+3okX6xluC0wXGEx43lpU/5KarfEGF/fRicoJ7wQY0+y5S92yTVjiihDmhC9xuoQy2LwJeM9/wqCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTExMDkyMTIyMTA0MlowIwYJKoZIhvcNAQkEMRYEFJmffM8fp7WkxbFeYd+YqbtXvhHqMA0GCSqGSIb3DQEBAQUABIGAI68+6F23KtUtX16YWQrmZ6KN9tsU5pMAwVI+x59CNB/9azClLeBEP1IjjtQoX0QJsVCBHNzOyUDYUm/JgViid+AlU/kqY2M9bcnkb87XVg8UuieDO6L04kf3FWKiMB/Z8BRgRWpIoz93bk1Vq/clg1hccHmNSG1pFfhTJkbAI1M=-----END PKCS7-----"><input type="image" src="https://www.paypalobjects.com/en_US/PL/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" width="1" height="1"></form>
+  </p>
+
+  <p>You can donate through <a href="https://www.paypal.com/">PayPal</a>.</p>
+
+  <p>The suggested amounts to donate are 5 / 10 / 20 USD, but feel free to donate
+  as much or as little as you like :) You can pay using your credit card,
+  or use your own PayPal account.</p>
+  </td>
+
+<?php /* Allura doesn't support donations,
+  https://sourceforge.net/p/allura/tickets/2540/,
+  so comment out section below:
 
   <td>
   <p><a href="https://sourceforge.net/donate/index.php?group_id=200653"><img src="http://images.sourceforge.net/images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" /> </a></p>
@@ -60,6 +76,7 @@ and view3dscene, and our games :) Thanks!</p>
   <!--You can also write a comment (public or not) while donating.-->
   <!-- Place here a list of subscribers gen by SF once it's sensible -->
   </td>
+  */ ?>
 </tr></table>
 
 <?php castle_footer(); ?>
