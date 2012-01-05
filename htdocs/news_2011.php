2aaf46b09515645f6ea9731e0e9ed4dd7d58f418
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-01-05 03:12:51 +0000

    Add to news: Help item to package.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@10709 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/news_2011.php
+++ htdocs/news_2011.php
@@ -23,6 +23,8 @@ Anchor {
 Previously Anchor was ignored (hidden by TouchSensor), now it's treated like sibling to TouchSensor. So it can be activated, it's desciption is shown etc. Compatible with at least InstantReality.
 
 <li>Engine works fully under 64-bit Windows (Win64, Windows on x86_64). <a href="http://michalis.ii.uni.wroc.pl/castle-engine-snapshots/">Snapshots are build for win-x86_64</a> too. If there's interest (please report on forum) we may release binaries for this system on next view3dscene release. (I don't think it's terribly important, because our 32-bit binaries actually work on Win64 flawlessly too.)
+
+<li>To our "Helping in the engine development" added "For Linux distros package maintainers". Please help creating a view3dscene package for popular Linux distributions!
 */
 
 array_push($news,
