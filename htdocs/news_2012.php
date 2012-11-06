6fbc84b86c89a76f2b43dfc37862b3f59a097baa
Michalis Kamburelis <michalis.kambi@gmail.com>
2012-11-06 21:36:38 +0000

    Make all player/creature FallXxx constants, and also the fall sound name, configurable by player.xml / resource.xml files.
    Also, removing fall_down_life_loss_scale: fall/damage/scale_min and fall/damage/scale_max replace it.
    
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@11948 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/news_2012.php
+++ htdocs/news_2012.php
@@ -64,6 +64,8 @@ You can registed own callbacks, see PlaceholdersNames and docs of TPlaceholderNa
     <li>TInventoryItem.Stack method to override, to affect how items stacking works.
     <li>TPlayer.Flying and TPlayer.FlyingTimeOut, a simpler and more flexible properties to control player flying (replace previous TPlayer.FlyingMode and friends).
 
+<li>New nicer properties to control middle point used for collisions (see T3D.Middle, T3DCustomTransform.MiddlePoint, T3DCustomTransform.PreferredHeight). Unified gravity on items and creatures, with new comfortable properties (see T3DCustomTransform.Gravity, T3DCustomTransform.FallSpeed, T3DCustomTransform.GrowSpeed). More configurable from resource.xml file (middle_height, fall_speed, grow_speed, direction_fall_speed --- see README_about_index_xml_files.txt).
+
 --
 for final 4.0.0 release?
 lights_editor_shadow_maps.png Lights Editor playing with shadow maps
