e89164e5f42437ee32a6527fe41f59124cd55a45
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-01-24 14:23:42 +0000

    Finish creating_data_levels.php .
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@12338 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/creating_data_levels.php
+++ htdocs/creating_data_levels.php
@@ -55,13 +55,92 @@ with links to documentation for every attribute.
 These are special 3D shapes that will be recognized by the engine to indicate:
 
 <ul>
-  <li>initial creatures / items positions by placeholders named "CasRes...",
-  <li>water volume by placeholder "CasWater",
-  <li>move limit by placeholder "CasMoveLimit",
-  <li>sectors/waypoints (to make creature AI smarter)
-    by placeholders "CasSector..." and "CasWaypoint..."
-  <li>see <?php api_link('TGameSceneManager.LoadLevel', 'CastleLevels.TGameSceneManager.html#LoadLevel'); ?> documentation for full list.
-  <li>and possibly your <?php api_link('TLevelLogic', 'CastleLevels.TLevelLogic.html'); ?>
+  <li><p>Initial <b>creatures / items</b> are indicated by placeholders named <tt>CasRes</tt>
+    + resource name. <tt>CasRes</tt> is short for <i>Castle Game Engine Resource</i>.
+
+    <p>The resource name refers to
+    <?php api_link('T3DResource.Name', 'CastleResources.T3DResource.html#Name'); ?>,
+    it much match one of <tt>name="ResourceName"</tt> declarations in your
+    <tt>resource.xml</tt> files.
+
+    <ul>
+      <li>To place a creature on the level, name your placeholder
+        <tt>CasRes&lt;resource-name&gt;[&lt;optional-initial-life&gt;][_&lt;ignored&gt;]</tt>.
+        By default (if not explicitly specified),
+        the initial creature life is taken from <tt>default_max_life</tt>
+        given in <tt>resource.xml</tt>.
+
+      <li>To place an item on the level, name your placeholder
+        <tt>CasRes&lt;resource-name&gt;[&lt;optional-item-quantity&gt;][_&lt;ignored&gt;]</tt>.
+        By default (if not explicitly specified),
+        item quantity is 1.
+    </ul>
+
+    <p>Anything after underscore is ignored. You can use this to make
+    placeholder name unique, e.g. all objects in Blender must be unique.
+    Note that Blender exporter changes the "dot" inside object names
+    to an underscore when exporting, so everything in Blender object name
+    after dot is ignored too.
+
+  <li><p><b>Water</b> volume by placeholder "CasWater"
+    (see <?php api_link('TCastleSceneManager.Water', 'CastleSceneManager.TCastleSceneManager.html#Water'); ?>).
+
+  <li><p><b>Move limit</b> by placeholder "CasMoveLimit"
+    (see <?php api_link('TCastleSceneManager.MoveLimit', 'CastleSceneManager.TCastleSceneManager.html#MoveLimit'); ?>).
+
+  <li><p><b>Sectors / waypoints</b> to improve creature AI moving.
+    Each sector occupies some volume in 3D (like a room).
+    Each waypoint indicates a point to pass when moving
+    from one sector to another (like a narrow door between two rooms).
+    Sectors create a graph, with waypoints indicating the graph
+    connections.
+    If the creature is in a different sector then it's target,
+    it walks through appropriate waypoints.
+
+<!--
+        <p>We may also use sectors to speed up rendering in the future.
+        For now, it doesn't seem needed &mdash; frustum culling,
+        and hardware occlusion query optimize the rendering set good enough,
+        without any difficult configuration.
+
+        It's assumed that there will not be too many sectors on the level
+        (100 sectors is <i>really</i> around maximum. 10-20 is reasonable.)
+        So sectors are <i>not</i> (at least right now) stored in any "intelligent"
+        structure (like an octree) and creature moving AI uses
+        the simplest search on the graph to find a satisfiable path
+        (no A* algorithm or anything; it's simply not needed for the kind of
+        sectors layouts that I will typically use).
+-->
+
+    <p>Placeholders named <tt>CasSector&lt;index&gt;[_&lt;ignored&gt;]</tt>
+    define sectors.
+    Placeholders named <tt>CasWaypoint[_&lt;ignored&gt;]</tt> define waypoints.
+
+    <p>Sectors of waypoints (and reverse property, waypoints
+    of sectors) are automatically calculated, by looking how waypoints
+    bounding boxes collide with sectors.
+    This is the only moment when waypoints bounding volumes are considered,
+    for all other purposes waypoints are simply 3D points.
+    You should place boxes that indicate waypoints
+    between two neighboring sectors, such that the bounding box
+    of the waypoint is partially inside both sectors.
+
+    <p>Sectors boxes need <i>not</i> be strictly separated.
+    When 3D object, like player or a creature, is within two sectors,
+    it's arbitrarily assigned to any of the possible sectors.
+    However, for creature AI, this may cause some awkward movement
+    (when the creature goes to a waypoint, instead of directly to the target),
+    so try to set sectors that don't overlap (much).
+
+    <p>You don't have to cover whole level with sectors.
+    If a creature (or it's target) is not inside any sector, then
+    the move direction is simply set to go to the target directly.
+  </li>
+
+  <li><p>See <?php api_link('TGameSceneManager.LoadLevel', 'CastleLevels.TGameSceneManager.html#LoadLevel'); ?>
+    documentation for full list of placeholders.
+
+  <li><p>And possibly your <?php api_link('TLevelLogic', 'CastleLevels.TLevelLogic.html'); ?>
     will define even more placeholders
     (by overriding <?php api_link('TLevelLogic.Placeholder', 'CastleLevels.TLevelLogic.html#Placeholder'); ?>,
     and using your logic as <?php api_link('LogicClass', 'CastleLevels.TLevelInfo.html#LogicClass'); ?>.
@@ -87,115 +166,16 @@ These are special 3D shapes that will be recognized by the engine to indicate:
     of course!).
 </ol>
 
-------------------------------------------------------------------------------
-TODO: old castle-development text, to be simplified
-
-<ul>
-  <li><p>To place items and creatures (collectively called "3D resources")
-    on the level you place a special "placeholder" objects on the level.
-    Every placeholder object will be removed
-    when the level is loaded (so you can use any shape for it &mdash;
-    I usually use wire cubes). Placeholder object position and name will
-    determine the actual item/creature position, resource (this refers
-    to a resource defined by resource.xml file, which in turn defines
-    the ObjectPascal class (determining the behavior of creature/item)
-    and a lot of properties) and quantity
-    (in case of items) or initial life (in case of creatures).
-    Name of the placeholder is
-    <tt>CasRes&lt;resource-name&gt;[&lt;resource-number&gt;][_&lt;ignored&gt;]</tt>:
-    <ul>
-      <li><tt>CasRes</tt> is just a shortcut for "<b>Cas</b>tle Game Engine <b>Res</b>ource".
-      <li><tt>&lt;resource-name&gt;</tt> is one of the resource names,
-        see available names in <tt>resource.xml</tt> files inside <tt>creatures/</tt>
-        and <tt>items/</tt> subdirectories.
-      <li><tt>&lt;resource-number&gt;</tt> (optional) is an integer
-        specifying item quantity (default 1 if not given) or initial creature
-        life (default is taken from <tt>resource.xml</tt> file, specific
-        to this creature).
-      <li>Anything after underscore is ignored. You can use this to make
-        object name unique, e.g. all Blender objects must be unique.
-        Note that Blender exporter changes the "dot" inside object names
-        to an underscore when exporting, so everything in Blender object name
-        after dot is ignored too.
-    </ul>
-
-  <li><p>When loading level, we search for Blender object named <tt>CasMoveLimit</tt>.
-    Our code is adapted to standard VRML/X3D exporters from Blender such that
-    we know how the name is stored inside VRML/X3D and we can retrieve it.
-
-    <p>Such object (if found) is removed from level 3D model,
-    and it's bounding box is used to limit allowed player positions.
-    This can be used to disallow player to move to the very edge of
-    the level.
-
-    <p>Usually you will add <tt>CasMoveLimit</tt> object using Blender, you can also
-    use <?php echo a_href_page('view3dscene', 'view3dscene') ?> &mdash;
-    see command 'Console -&gt; Print scene bounding box'.
-
-  <li><p>Blender object named <tt>CasWater</tt> indicates water volume.
-    At some point this will be extended (when I'll need it) to
-    include every 'CasWater[_&lt;ignored&gt;]', so that you will be able to
-    define water by a sum of shapes. Right now, it's only a single shape,
-    and we look only at it's bounding box.
-
-  <li><p>Sectors and waypoints (aka portals) for the level:
-
-    <ul>
-      <li><p>Placeholders named CasSector&lt;index&gt;[_&lt;ignored&gt;]
-        define sectors.
-        Placeholders named CasWaypoint[_&lt;ignored&gt;] define sectors.
-        See TGameSceneManager.LoadLevel API docs for details.
-
-      <li><p>Sectors of waypoints (and reverse property, Waypoints
-        of sectors) are automatically calculated, by looking how waypoints
-        bounding boxes collide with sectors.
-        This is the only moment when waypoint's bounding volume are considered,
-        for all other purposes waypoints are simply points.
-        So you place boxes that indicate waypoints
-        between two neighboring sectors, such that the bounding box
-        of the waypoint is partially inside both sectors.
-
-        <p>Sectors boxes need <i>not</i> be strictly separated.
-        When 3D object, like player or creature, is within two sectors,
-        it's arbitrarily assigned to any of the possible sectors.
-        However, for creature AI, this may cause some awkward movement
-        (when the creature goes to a waypoint, instead of directly to the target),
-        so try to set sectors that don't overlap (much).
-
-      <li><p>You don't have to cover whole level with sectors.
-        If creature (or it's target) is not inside any sector, then
-        the move direction is simply set to go to the target directly.
-
-      <li><p>Sectors and waypoints are used for creature AI.
-        Each sector occupies some space in 3D (like a room).
-        Each waypoint indicates a place where you can move
-        from one sector to another (like a narrow door between two rooms).
-        Sectors create a graph, with waypoints indicating the graph
-        connections.
-        If the creature is in a different sector then it's target,
-        it walks through appropriate waypoints.
-
-
-        <p>We may also use sectors to speed up rendering in the future.
-        For now, it doesn't seem needed &mdash; frustum culling,
-        and hardware occlusion query optimize the rendering set good enough,
-        without any difficult configuration.
-
-<!--
-        It's assumed that there will not be too many sectors on the level
-        (100 sectors is <i>really</i> around maximum. 10-20 is reasonable.)
-        So sectors are <i>not</i> (at least right now) stored in any "intelligent"
-        structure (like an octree) and creature moving AI uses
-        the simplest search on the graph to find a satisfiable path
-        (no A* algorithm or anything; it's simply not needed for the kind of
-        sectors layouts that I will typically use).
--->
-      </li>
-    </ul>
-  </li>
-</ul>
-------------------------------------------------------------------------------
-
 <?php
 creating_data_footer();
 ?>
+
+
+
+
+
+
+
+
+
+
