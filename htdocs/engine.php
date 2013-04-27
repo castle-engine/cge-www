cd3070cb4caf541b18d067041f97bd45f27b4295
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-04-27 19:53:18 +0000

    Update FPC version notes.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@12658 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/engine.php
+++ htdocs/engine.php
@@ -367,12 +367,12 @@ you have appropriate libraries installed on your system.
 <?php echo $toc->html_section(); ?>
 
 <p>You need the <a href="http://www.freepascal.org/">Free Pascal Compiler
-(FPC)</a> to use our engine. You may also find it comfortable
-to use <a href="http://lazarus.freepascal.org/">Lazarus</a>,
-which is an IDE (editor, debugger etc.) built around FPC.</p>
-
-<p><b>We always support and advice the latest stable release of FPC
-(currently 2.6.0).</b>.</p>
+(FPC)</a> to use our engine.
+<b>We always support and advice the latest stable (from
+<a href="http://www.freepascal.org/">freepascal.org</a>)
+release of FPC (currently 2.6.2).</b>
+Usually, we also support a couple of older FPC releases.
+Currently (for engine 4.0.1) <b>FPC versions &gt;= 2.6.0 are supported</b>.
 
 <!--
 <p><i>Note only for FPC 2.6.0:</i> if you use <tt>compile.sh</tt> scripts,
@@ -383,12 +383,34 @@ This isn't a problem for earlier FPC versions, or if you compile programs
 with Lazarus.
 -->
 
-<p>Usually, we also support a couple of older FPC releases.
-But currently (for engine 4.0.0) only <b>FPC versions &gt;= 2.6.0 are supported</b>
-(because of a couple of very useful new language features).</p>
+<p>You may also find it comfortable
+to use <a href="http://lazarus.freepascal.org/">Lazarus</a>,
+which is an IDE (editor, debugger etc.) built around FPC
+with a visual classes library (LCL).
+Our engine components can be used together with Lazarus forms
+(although we also have an alternative window classes, independent from
+Lazarus LCL).
+<b>Any Lazarus version based on FPC &gt;= 2.6.0 is supported now.</b></p>
 
 <!--p>I also regularly test FPC from SVN,
 so it's usually painless to use even development FPC releases.</p-->
+
+<!-- The policy for choosing FPC releases:
+1. Advice and support latest FPC release from freepascal.org.
+2. Try to support a couple of older FPC releases, as much as is reasonable.
+The definition what is "reasonable" depends on:
+1. What FPC version is inluded in distros (in particular, what is in
+   last Debian stable and last Ubuntu LTS).
+2. Do we need some new language features available in new FPC versions.
+   For example, we heavily use FPC generics since some time, they make
+   a lot of code very nice and clean, but they also bumped our FPC requirements
+   at some point.
+   We also use "nested of" construct of FPC 2.6.0, this again allows for
+   much cleaner code, and also more optimized in one important case.
+When our engine/view3dscene will be officially included in distros,
+I would make the point 1. more influencial, and make everything possible
+to hang on to FPC releases available in distros.
+-->
 
 <?php echo $toc->html_section(); ?>
 
