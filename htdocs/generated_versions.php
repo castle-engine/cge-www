6feb872d2046a0fdddc1557b910b2393f18b17d5
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-08-17 19:32:43 +0000

    Update WWW and versions for engine 4.1.1 release.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@13050 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/generated_versions.php
+++ htdocs/generated_versions.php
@@ -11,6 +11,6 @@
   define('VERSION_GLINFORMATION', '1.2.2');
   define('VERSION_GLINFORMATION_GLUT', '1.2.2');
   define('VERSION_GEN_FUNCTION', '1.0.5');
-  define('VERSION_CASTLE_GAME_ENGINE', '4.1.0');
+  define('VERSION_CASTLE_GAME_ENGINE', '4.1.1');
   define('VERSION_DEMO_MODELS', '3.3.0');
 ?>
