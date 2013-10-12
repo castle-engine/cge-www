feded36cd0d3ab10d85a15a345a22b84731eebf7
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-10-12 21:34:32 +0000

    Clarify the license of "The Castle" - start. Cleanup castle-credits.php , and gather all known info (and mark unknown) in castle/data/AUTHORS.txt .
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@13224 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/castle-credits.php
+++ htdocs/castle-credits.php
@@ -6,11 +6,9 @@
   $toc = new TableOfContents(
     array(
       new TocItem('Authors', 'authors'),
-      new TocItem('Other used things', 'used_things'),
-      new TocItem('Code', 'code', 1),
-      new TocItem('Graphics', 'graphics', 1),
-      new TocItem('Sounds', 'sounds', 1),
-      new TocItem('DOOM E1M1 bonus level', 1),
+      new TocItem('Software used when developing this game', 'software_used'),
+      new TocItem('Data used in this game', 'data_used'),
+      new TocItem('DOOM E1M1 bonus level', 'doom', 1),
     )
   );
 ?>
@@ -22,150 +20,70 @@
 
 <?php echo $toc->html_section(); ?>
 
-<p>Programming and modelling (of everything not covered below) :
-<a href="http://michalis.ii.uni.wroc.pl/~michalis/">Michalis Kamburelis</a>.
+<p><a href="http://michalis.ii.uni.wroc.pl/~michalis/">Michalis Kamburelis</a>
+did the whole programming and most of the 3D modelling.
 
-<p><a href="http://stoma.bestweb.pl/">Szymon Stoma and Kaśka Zaremba</a>
-designed and modelled the scenario
+<p><i>Szymon Stoma and Kaśka Zaremba</i> designed and modelled the scenario
 on "The Gate" level (all models in <tt>data/levels/gate/gate_parts/</tt>),
-modelled various objects (keys, bow, quiver, arrows, ball_missile_s),
-provided some sounds and a large amount of feedback. Thanks!
-<!-- All their work is on GNU GPL,
-contact Szymon &amp; Kaśka</a>
-for other licensing.
---></p>
+modelled many 3D objects (keys, bow, quiver, arrows, ball_missile_s),
+provided some sounds and a large amount of feedback.</p>
 
-<p><a href="http://sourceforge.net/users/herrmannek/">Grzegorz Hermanowicz
-(herrmannek)</a> implemented underwater "sick"
-projection effect and started gravity for arrows implementation,
-thanks ! Finally we have a 2nd guy with SVN write access,
-so if something gets broken &mdash; that's totally not me :)</p>
+<p><i>Grzegorz Hermanowicz</i> implemented underwater "sick"
+projection effect and started gravity for arrows implementation.</p>
 
-<?php echo $toc->html_section(); ?>
+<p><i>Michał Wojtkiewicz</i> provided the <tt>levels/gate/tree</tt> model.</p>
+
+<p><i>Great thank you to everyone who contributed!</i></p>
 
 <?php echo $toc->html_section(); ?>
 
 <ul>
-  <li><?php echo a_href_page('Castle Game Engine', 'engine'); ?>
-    is the basis of this game.</li>
+  <li><p><?php echo a_href_page('Castle Game Engine', 'engine'); ?>.
+    The base of our game.
 
-  <li><a href="http://www.freepascal.org/">FreePascal</a> is a compiler
-    that made this all possible.</li>
+    <p>We also used some engine tools and accompanying programs:
+    <?php echo a_href_page('rayhunter', 'rayhunter'); ?>
+    was used to render some items 2D images (see <tt>data/items/xxx/image.png</tt>),
+    <?php echo a_href_page('view3dscene', 'view3dscene'); ?>
+    was used to test and slightly edit 3D models,
+    and to make many screenshots.
+  </li>
+
+  <li><a href="http://www.freepascal.org/">Free Pascal Compiler</a></li>
 </ul>
 
-<?php echo $toc->html_section(); ?>
-
+<p>Graphics:
 <ul>
-  <li><tt>data/levels/gate/cart</tt> model was
-    found among the first 10 google results for "free 3d models".
-    Terms are some open license (like artistic or gpl).</li>
-
-  <li><tt>tree.blend</tt> based on <tt>tree02.3DS</tt>
-    from Michał Wojtkiewicz. Thanks!</li>
-
-  <li>Werewolf and Alien creatures' models based on free models from
-    <a href="http://www.3dcafe.com/">www.3dcafe.com</a>.
-    Spider's queen face also based on model from this site.</li>
-
-  <li>Ghost's teeth are based on vampire teeth from
-    <a href="http://www.katorlegaz.com/">www.katorlegaz.com</a>
-    from characters/vamp.zip model.</li>
-
   <li><a href="http://www.blender3d.org/">Blender</a></li>
   <li><a href="http://www.gimp.org/">GIMP</a></li>
   <li><a href="http://www.imagemagick.org/script/index.php">ImageMagick</a></li>
 
   <li><a href="http://www.planetside.co.uk/terragen/">Terragen</a>
-    was used to create sky (see data/skies/).
-    This is the only non-open-source component I used... pity :(.
-    But I don't know of any (open-source or not) alternative to Terragen.</li>
+    was used to create skies (see <tt>data/skies/</tt>).
 
-  <li>Many textures from <a href="http://www.wolfiesden.com/golgotha/golgotha.asp">public
-    domain Golgotha textures</a></li>
+  <li>Some post-processing of VRML models was done using EmacsLisp.
+    See <tt>data/kambi-castle-utils.el</tt> and various Makefiles,
+    like <tt>data/levels/gate/Makefile</tt>.
+</ul>
 
-  <li>Many textures from <a href="http://www.lemog.fr/lemog_textures/index.php">LeMog - 3d Textures</a>
+<p>Sounds:
+<ul>
+  <li><a href="http://www.metadecks.org/software/sweep/">Sweep</a>
+    and <a href="http://audacity.sourceforge.net/">Audacity</a> were used
+    to edit sounds.</li>
 
-  <li>Some post-processing of VRML models is done using EmacsLisp,
-    see <tt>data/kambi-castle-utils.el</tt> and various Makefiles,
-    like <tt>data/levels/gate/Makefile</tt>
-
-  <li>stars.jpg <a href="http://www.astrooptik.com/Bildergalerie/latest_namibia/pictures.htm">grabbed
-    from here</a>
-
-  <li>Some of my other programs dealing with VRML were also used:
-    <?php echo a_href_page('rayhunter', 'rayhunter'); ?>
-    was used to render items 2D images (see <tt>data/items/xxx/image.png</tt>),
-    <?php echo a_href_page('view3dscene', 'view3dscene'); ?>
-    was often used to see how VRMLs look
-    (independent of how they look when used in "The Castle"),
-    and to generate some VRML code pieces, and some screenshots.</li>
+  <li><a href="http://sox.sourceforge.net/">Sox</a> was used
+    for some batch processing of sounds.
 </ul>
 
 <?php echo $toc->html_section(); ?>
 
-<ul>
-  <li>I use <a href="http://www.metadecks.org/software/sweep/">Sweep</a>
-    and <a href="http://audacity.sourceforge.net/">Audacity</a> to edit
-    sounds.</li>
-  <li><a href="http://sox.sourceforge.net/">Sox</a> is also a great program,
-    suitable for batch processing of sounds.
-
-  <li>Many freeware sounds from
-    <a href="http://www.flashkit.com/soundfx/">www.flashkit.com/soundfx/</a>.
-
-  <li>Sound loops (i.e. game music):
-
-    <p>Found by
-      <a href="http://www.flashkit.com/loops/">www.flashkit.com/loops/</a>.
-      These loops are really great so I give here a link to all
-      author's websites &mdash; if I'll forget some loop please remind me.
-
-      <table border="1">
-        <tr>
-          <th>Place in this game</th>
-          <th>Title and URL to flashkit</th>
-          <th>Author</th>
-        </tr>
-
-        <tr>
-          <td>Intro (initial menu)</td>
-          <td>(don't remember)</td>
-          <td><a href="http://www.edgen.com/">edgen</a></td>
-        </tr>
-
-        <tr>
-          <td>"The Gate" level</td>
-          <td>(don't remember)</td>
-          <td><a href="http://calpomatt.lavista.org/">calpomatt</a></td>
-        </tr>
-
-        <tr>
-          <td>Castle Hall" level</td>
-          <td><a href="http://www.flashkit.com/soundfx/Ambience/TEMPLE-Adam_Goh-7394/index.php">Temple</a></td>
-          <td><a href="http://www.sades.com/">Adam Goh</a></td>
-        </tr>
-
-        <tr>
-          <td>"Cages" level</td>
-          <td><a href="http://www.flashkit.com/loops/Ambient/Soundscapes/Cath-Mark-9479/index.php">Cathedral</a></td>
-          <td><a href="http://reaction.roxr.com/">Mark</a></td>
-        </tr>
-
-        <tr>
-          <td>Game finish sequence</td>
-          <td><a href="http://www.flashkit.com/loops/Ambient/Ambient/Quiet-calpomat-5053/index.php">Quiet</a></td>
-          <td><a href="http://calpomatt.lavista.org/">calpomatt</a></td>
-        </tr>
-      </table>
-  </li>
-
-  <li><tt>menu_current_item_changed.wav</tt> based on
-    <tt>/usr/share/sounds/KDE_TypeWriter_Key_2.ogg</tt>
-  <li><tt>werewolf_howling.wav</tt> from
-    <a href="http://www.acs.ucalgary.ca/~mmusiani/">here</a>.
-  <li><tt>tomb.wav</tt> from
-    <a href="http://rasmus.uib.no/~st01369/filarkiv/lyder.html">here</a>
-</ul>
+<p>While most of the data used by the game was done
+by "The Castle" team (see <a href="#section_authors">above</a>),
+some data was obtained from the Internet,
+under various "free" licenses. Unfortunately, not 100% of them are really
+FOSS compatible, but we're working on it.
+See <tt>castle/data/AUTHORS.txt</tt> for details.
 
 <?php echo $toc->html_section(); ?>
 
@@ -187,7 +105,7 @@ I and II Music" as compiled by Paul Burdette</a>.
   <p><i>Legal things</i>:
 
   <p><b>For distro packagers</b>: remember that the Doom level and it's resources
-  (i.e. most of the things inside directories <tt>data/levels/doom/</tt>,
+  (i.e. most of the things inside directories <tt>data/levels/doom/e1m1/</tt>,
   <tt>data/textures/doom/</tt> and <tt>data/sounds/doom/</tt>)
   are not open-source, are not
   covered by GNU GPL license, are not under my (Michalis Kamburelis)
