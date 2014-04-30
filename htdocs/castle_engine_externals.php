f7445e65f25906f0430047f1c95ea683db97b095
Michalis Kamburelis <michalis.kambi@gmail.com>
2014-04-30 17:53:44 +0000

    Fix FB like box.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@13816 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/castle_engine_externals.php
+++ htdocs/castle_engine_externals.php
@@ -95,7 +95,7 @@ function facebook_button()
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, \'script\', \'facebook-jssdk\'));</script>
 ';
