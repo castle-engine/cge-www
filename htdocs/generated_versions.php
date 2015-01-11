e1d992a9eca7ac1446168c9670b26cdaedbb1a83
Michalis Kamburelis <michalis.kambi@gmail.com>
2015-01-11 07:12:06 +0000

    Bump engine version to 5.1.2.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@14362 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/generated_versions.php
+++ htdocs/generated_versions.php
@@ -12,6 +12,6 @@
   define('VERSION_GEN_FUNCTION', '1.0.5');
   define('VERSION_MOUNTAINS_OF_FIRE', '1.1.0');
   define('VERSION_GLINFORMATION_GLUT', '1.2.2');
-  define('VERSION_CASTLE_GAME_ENGINE', '5.1.1');
+  define('VERSION_CASTLE_GAME_ENGINE', '5.1.2');
   define('VERSION_DEMO_MODELS', '3.5.0');
 ?>
