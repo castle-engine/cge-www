3b91109b1db12e010058d854da670c25630eefde
Michalis Kamburelis <michalis.kambi@gmail.com>
2015-06-13 17:09:24 +0000

    On Windows, log to xxx.log file for GUI programs.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@14556 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/tutorial_log.php
+++ htdocs/tutorial_log.php
@@ -6,13 +6,23 @@ tutorial_header('Logging');
 <p>Simply use <?php api_link('CastleLog', 'CastleLog.html'); ?> unit and call
 <?php api_link('InitializeLog', 'CastleLog.html#InitializeLog'); ?>
  to have logging enabled.
-Log by default goes to <i>standard error output</i> (<code>StdErr</code>,
-also known as <code>ErrOutput</code> in Pascal), which is
-standard on Unixes (and avoids users asking questions <i>"where is the log
-file"</i>). Be careful on Windows, for GUI applications the <code>StdErr</code>
-may be undefined &mdash; you can either suggest users to run your program from
-command-line to get the log, or you can pass a parameter to <code>InitializeLog</code>
-to generate log to any stream (like to a temporary file).
+
+<ul>
+  <li><p><i>On Unix (Linux, Mac OS X...), and on Windows console applications</i>,
+    logging by default goes to the <i>standard error output</i>.
+    This is called <code>StdErr</code>, also known as <code>ErrOutput</code> in Pascal.
+    This is the standard behaviour for Unix and console apps.
+    The nice thing about it is that it avoids users asking questions <i>"where is the log
+    file"</i> &mdash; because they choose the name and location of the log file themselves.
+
+  <li><p><i>On Windows GUI applications</i>
+    <!-- the standard error is (usually) not available, so -->
+    we log to the file <code>xxx.log</code> in the current directory
+    (where <code>xxx</code> is your <code>ApplicationName</code>).
+
+  <li><p>You can pass a parameter to <code>InitializeLog</code>
+    to generate log to any stream.
+</ul>
 
 <p>A lot of engine components automatically use this log.
 To use it yourself, just call
