2edc1b19f3c6d31fab80feae52d3c16fbf9ce9b4
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-01-02 04:53:19 +0000

    Assume password is remembered.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@15027 afe8dcc5-9094-451b-8773-5f495042a737

--- scripts/sf_htdocs_update.sh
+++ scripts/sf_htdocs_update.sh
@@ -2,10 +2,9 @@
 set -eu
 
 SF_USERNAME="kambi"
-SVN_PASSWORD="$1"
 
 ssh "$SF_USERNAME",castle-engine@shell.sourceforge.net create
 ssh "$SF_USERNAME",castle-engine@shell.sourceforge.net <<EOF
 cd /home/project-web/castle-engine/htdocs/
-svn up --password="${SVN_PASSWORD}"
+svn up
 EOF
