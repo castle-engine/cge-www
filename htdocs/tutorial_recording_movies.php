38eca1365a4af4f0bbed060d16a2b80d96faeb69
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-02-08 15:01:09 +0000

    gitignore, and updated tutorial_recording_movies.php.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/www@15126 afe8dcc5-9094-451b-8773-5f495042a737

--- htdocs/tutorial_recording_movies.php
+++ htdocs/tutorial_recording_movies.php
@@ -3,18 +3,29 @@ require_once 'castle_engine_functions.php';
 tutorial_header('Recording movies');
 ?>
 
-<p>Note that our engine doesn't support (yet) recording movies during
-interactive operations, like during a game. One reason for this is
-that it's non-trivial to implement, and the other reason is that you
+<p>Our engine does not include a ready API
+to record movies during interactive applications / games.
+This is not trivial to implement, and also you
 can use existing external programs to do this. Under Linux I like
 <a href="https://github.com/nullkey/glc/wiki/">glc</a>,
 under Windows there is <code>fraps</code> and many others.
 
-<p>However, we support recording a predefined animation in VRML/X3D
+<p>You can of course <?php echo a_href_page('grab and save a screenshot',
+'tutorial_screenshots'); ?> as often as you want.
+But doing this every frame will make your FPS very, very low &mdash;
+so it's not a good method to record a movie from a gameplay.
+
+<p>That said, you can use the screenshot method (<code>SaveScreen</code>)
+to record a <i>predefined animation</i>. That is, if you can programmatically
+animate your world and/or move the camera and do other necessary stuff,
+you can convert it into a perfect-quality movie.
+So we support recording a predefined animation in VRML/X3D
 (meaning: it plays without any user interaction) to a series of
 images. Such animation can then be converted into a movie e.g. using
 <code>ffmpeg</code> (our <?php echo a_href_page('view3dscene', 'view3dscene'); ?>
- does this automatically under the hood). Although this is
+ does this automatically under the hood).
+
+<p>Although this is
 limited to predefined animations, in exchange this allows to record
 the animation with perfect quality and with constant number of frames
 per second.
@@ -27,13 +38,16 @@ begin
   SceneManager.MainScene.IncreaseTime(FrameTime);
   SceneManager.Camera.Update(FrameTime, true, DummyBooleanInitializedToTrue);
 
+  // ... do anything else you need to update the 3D world/camera
+  // when the time passed
+
   { capture and save screen as before, like: }
   Image := Control.SaveScreen;
   try
     { add your image to the movie sequence.
-      Various options possible, view3dscene for now just saves a sequence
-      of images to temporary location and later processes them by ffmpeg
-      to produce a full movie. }
+      Various options possible, e.g. you can save a sequence
+      of images to a temporary location and later processes
+      them by ffmpeg to produce a full movie. }
     SaveImage(Image, Format(\'screenshot_name_%4d.png\', [FrameNumber]));
   finally FreeAndNil(Image) end;
 end;'); ?>
