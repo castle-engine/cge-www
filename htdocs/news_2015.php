2524f7b122610105ca660ebe69089775087c09dc
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-02-18 06:23:46 +0100

    Spelling fixes.
    Also, test GIT->SVN synchronization.

--- htdocs/news_2015.php
+++ htdocs/news_2015.php
@@ -131,7 +131,7 @@ OnMotion: TControlInputMotionEvent
 OnUpdate: TNotifyEvent
 </pre>
 
-     <p>These should be used to watch open/close of context, to watch key/mouse events and to perform continous updates. For detailed documentation of them, see <a href="http://castle-engine.sourceforge.net/apidoc/html/CastleControl.TCastleControlCustom.html">TCastleControlCustom</a> reference.</p>
+     <p>These should be used to watch open/close of context, to watch key/mouse events and to perform continuous updates. For detailed documentation of them, see <a href="http://castle-engine.sourceforge.net/apidoc/html/CastleControl.TCastleControlCustom.html">TCastleControlCustom</a> reference.</p>
 
     <p>Some previously published stuff on <code>TCastleControl</code> is deprecated now and will be removed in next release (sorry, we have to break compatibility &mdash; this old stuff really doesn\'t make much sense, all new code should use new events). Also, the deprecated <code>OnPaint</code> was fixed &mdash; just in case you\'re using it (but please switch to using <code>OnRender</code> soon!).
 
@@ -139,7 +139,7 @@ OnUpdate: TNotifyEvent
 
     <p>Also, the tutorial chapter <a href="http://castle-engine.sourceforge.net/tutorial_castle_scene_transform.php">"Adding a simple moving object" was added</a>.
 
-  <li>Also, as you probaly see, the website got a total facelift, using HTML5 and Bootstrap style. Various pages and menus were rearranged to be more helpful. I hope you enjoy the new website &mdash; please leave a note in the comments :)
+  <li>Also, as you probably see, the website got a total facelift, using HTML5 and Bootstrap style. Various pages and menus were rearranged to be more helpful. I hope you enjoy the new website &mdash; please leave a note in the comments :)
 
   <li>You can save almost 0.7 MB from exe size by undefining CASTLE_EMBED_ALL_3D_FONT_VARIATIONS in some cases, see src/base/castleconf.inc for comments.
 
