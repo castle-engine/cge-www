33ea381f79780e7b0c65e71279dd064ccbfe3140
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-05-29 22:05:54 +0200

    LineProperties support comments

--- htdocs/x3d_implementation_shape.php
+++ htdocs/x3d_implementation_shape.php
@@ -11,9 +11,43 @@
 <ul>
   <li><p><?php echo x3d_node_link('Shape'); ?>,
     <?php echo x3d_node_link('Appearance'); ?>,
-    <?php echo x3d_node_link('Material'); ?></p></li>
+    <?php echo x3d_node_link('Material'); ?></p>
 
-  <li><p><b>VRML 1.0 and multiple materials</b>: multiple materials
+    <p>Fully implemented.</li>
+
+  <li><p><?php echo x3d_node_link('LineProperties'); ?>.
+    Fullly implemented.
+
+    <p>Allows to change line width and type (pattern).
+    Suitable for all line geometry,
+    in particular <code>IndexedLineSet</code> and <code>LineSet</code>.
+    It also affects normal (filled) geometry (like <code>IndexedFaceSet</code>)
+    when viewed in wireframe mode (see view3dscene "View -&gt; Fill Mode"
+    menu).</p>
+
+    <p>We only support values 1..5 (<i>Solid</i> .. <i>Dash-dot-dot</i>) for <code>linetype</code> field.
+
+    <p>We use the GPU (OpenGL/OpenGLES) features to render lines with
+    custom width and pattern (type).
+
+    <ul>
+      <li><p>This has the advantage that it's ultra-fast. Changing line width or
+        using line pattern has virtually zero cost.
+
+      <li><p>But the disadvantage is that we only support what the GPU (OpenGL/OpenGLES) supports.
+
+        <p>This particularly hurts on OpenGLES (Android, iOS), where
+
+        <ol>
+          <li><code>glLineStipple</code> is not supported (so X3D <code>linetype</code> field will unfortunately do nothing).
+
+          <li>Line width is limited. You can see the maxium line width on the console (output of <code>GLInformationString</code>), as <i>"Max line width"</i>. Unfortunately, on many Android devices it's just 1. Which means that <code>glLineWidth</code> is supported, but actually completely ignored by the device.
+        </ol>
+
+        <p>If you need 100% reliable line width or patterns on Android/iOS, you may need to render them differently, using filled rectangles (e.g. using <code>IndexedFaceSet</code> or <code>Rectangle2D</code> nodes). To simulate line patterns, use small repeatable textures with transparent pixels (you can set texture filtering to <code>"NONE"</code> using <code>TextureProperties</code> node to avoid smoothing the alpha channel).
+    </ul>
+
+  <li><p><b>Notes about VRML 1.0 and multiple materials</b>: multiple materials
     within a single VRML 1.0 <code>Material</code> node work 100%
     correctly if you change only emissive and transparency,
     or only diffuse and transparency for each index.
@@ -33,13 +67,6 @@
     saner <code>Color</code> and <code>ColorRGBA</code> nodes, that are implemented
     fully.</p>
 
-  <li><p><?php echo x3d_node_link('LineProperties'); ?>.
-    Implemented, allows to change line width and type (pattern).
-    Suitable for all line geometry,
-    in particular <code>IndexedLineSet</code> and <code>LineSet</code>.
-    It also affects normal (filled) geometry (like <code>IndexedFaceSet</code>)
-    when viewed in wireframe mode (see view3dscene "View -&gt; Fill Mode"
-    menu).</p>
 </ul>
 
 <p><i>TODO</i>: FillProperties, TwoSidedMaterial are missing.</p>
