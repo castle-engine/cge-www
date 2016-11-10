ac0e9d75269d81b8f68a005fd59617741ddef9c0
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-11-10 04:20:10 +0100

    Reworked and improved vrml_x3d.php

--- htdocs/vrml_x3d.php
+++ htdocs/vrml_x3d.php
@@ -15,39 +15,71 @@
   echo pretty_heading($page_title);
 ?>
 
-<p>The engine core is a <i>scene graph</i> using nodes defined by the X3D specification.
+<!--p>The engine core is a <i>scene graph</i> using nodes defined by the X3D specification.-->
 
-<p>Simply put, X3D (and it's older version, VRML) is a file format for 3D models.
+<p>Simplifying, X3D (and it's older version, VRML) is a file format for 3D models.
 You will find that virtually any 3D modeling program can export to it,
 for example <a href="http://www.blender.org/">Blender</a> includes
 an X3D exporter (see also <?php echo
 a_href_page('our Blending exporting notes', 'creating_data_blender'); ?>).</p>
 
-<p>To start the fun, just create some VRML/X3D models
+<p>To start the fun, just create some X3D models
 (or download them from the Internet, or grab our
-<?php echo a_href_page('VRML/X3D demo models', 'demo_models'); ?>)
+<?php echo a_href_page('demo models', 'demo_models'); ?>)
 and open them with our
 <?php echo a_href_page('view3dscene', 'view3dscene'); ?>.</p>
 
-<p>As a 3D file format, VRML/X3D is quite unique because it's designed
-to describe <i>virtual 3D worlds</i>, not just static scenes.
-So you can do animations, interactive behaviors (e.g. open the door
-when user presses a handle), and scripting right inside the VRML/X3D file.
-Many advanced graphic effects are also possible, like making
-mirrors by generated (cube map or flat) textures, using GLSL shaders
-and much more.</p>
+<p>As a 3D file format, X3D is quite unique, as</p>
 
-<p>The <b><a href="http://web3d.org/x3d/specifications/">specifications
-for X3D (and older VRML) are available here</a></b>.
-This is your ultimate resource to learn what you can do with VRML/X3D.
-The older versions were called VRML (VRML 1.0, then VRML 2.0 also
-known as VRML 97). Newer versions are called X3D (3.x).
-I collectively call them all <i>VRML/X3D</i> because our engine handles
-all versions of them. You probably want to use the newest one,
-X3D, whenever possible.</p>
+<ul>
+  <li><p>It's not only a file format. It's actually a very flexible scene graph
+    for 3D applications. <!-- It is the way to build and modify
+    the 3D content in your applications. -->
+    Every X3D node corresponds to a Pascal class with appropriate fields,
+    and you can freely <a href="tutorial_scene.php#section_building_and_editing">create and modify X3D nodes at runtime</a>.
+    <!--
+    The fact that it has an associated file format
+    (actually, more than once &mdash; XML encoding, classic encoding...)
+    is just "an extra".--></li>
 
-<p>Pages in this section describe our handling of VRML/X3D:
-<?php echo a_href_page('how we handle the stuff in official VRML/X3D specification', 'x3d_implementation_status'); ?> and
-<?php echo a_href_page('what new features we add to VRML/X3D', 'x3d_extensions'); ?>.
+  <li><p>It's designed to describe <i>virtual 3D worlds</i>,
+    not just static scenes.
+    So you can express animations, interactive behaviors (e.g. open the door
+    when user presses a handle), and scripting right inside the X3D file.
+    Many advanced graphic effects are also possible, like
+    <a href="x3d_implementation_cubemaptexturing.php">mirrors by generated cube map textures</a>,
+    <a href="x3d_extensions_screen_effects.php">screen effects</a>,
+    <a href="x3d_extensions_shadow_maps.php">shadow maps</a>,
+    <a href="x3d_extensions_shadow_volumes.php">shadow volumes</a>,
+    <a href="compositing_shaders.php">effects using GLSL shaders</a>
+    and much more.</li>
+</ul>
+
+<p><b>Learning X3D</b>: Use this part of the documentation to learn
+about the available X3D nodes and their fields.</p>
+
+<ul>
+  <li><p><?php echo a_href_page('The nodes in the official X3D specification', 'x3d_implementation_status'); ?>
+    are divided into components. We list all the supported nodes,
+    with links to their X3D specification.
+    For some nodes, we also mention
+    eventual caveats or simple extensions that we have implemented.
+  </li>
+
+  <li><p><?php echo a_href_page('The unofficial nodes that we add', 'x3d_larger_extensions.php'); ?>
+    documents some cool graphic effects available in our engine
+    through special X3D nodes.
+  </li>
+
+  <li><p>In any case, the <a href="http://web3d.org/x3d/specifications/">X3D specifications</a> are your ultimate resource to learn what you can do with X3D.</p>
+
+    <!--
+    The older versions were called VRML (VRML 1.0, then VRML 2.0 also
+    known as VRML 97). Newer versions are called X3D (3.x).
+    I collectively call them all <i>X3D</i> because our engine handles
+    all versions of them. You probably want to use the newest one,
+    X3D, whenever possible.</p-->
+  </li>
+</ul>
 
 <?php vrmlx3d_footer(); ?>
