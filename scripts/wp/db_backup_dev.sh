48a6f27131161b84d7ef4740a8895d334eb85b24
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-02-15 15:15:49 +0100

    Backup scripts save backups to directory, under new names

--- scripts/wp/db_backup_dev.sh
+++ scripts/wp/db_backup_dev.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -eu
 
-mysqldump --events cgewp -u cgewp --password=devpassword | gzip -c > "$CASTLE_WORDPRESS_BACKUP_FILE"
+# Create each new backup under a new name, to keep history
+# (our DB is very small, so size is not a concern now).
+# Then, copy it to latest-db-backup.sql.gz, to easily restore from it later.
+
+BACKUP_FILE="$CASTLE_WORDPRESS_BACKUP_PATH"/`date '+%F_time-%H-%M-%S'`.sql.gz
+mysqldump --events cgewp -u cgewp --password=devpassword \
+  | gzip -c > "$BACKUP_FILE"
+cp -f "$BACKUP_FILE" "$CASTLE_WORDPRESS_BACKUP_PATH"/latest-db-backup.sql.gz
+
+echo 'Backups now:'
+ls -Flh "$CASTLE_WORDPRESS_BACKUP_PATH"
--- scripts/wp/db_restore_dev.sh
+++ scripts/wp/db_restore_dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -eu
 
-gunzip -c "$CASTLE_WORDPRESS_BACKUP_FILE" | mysql cgewp -u cgewp --password=devpassword
+BACKUP_FILE="$CASTLE_WORDPRESS_BACKUP_PATH"/latest-db-backup.sql.gz
+gunzip -c "$BACKUP_FILE" | mysql cgewp -u cgewp --password=devpassword
