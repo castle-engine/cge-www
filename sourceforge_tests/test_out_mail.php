1727e488dfbc5ebef36636b19fe5dbbb1f12ecfd
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-02-15 11:04:44 +0100

    Update script to test outgoing mail

--- sourceforge_tests/test_out_mail.php
+++ sourceforge_tests/test_out_mail.php
@@ -1,18 +1,19 @@
 <?php
 
-  /* Testing mail sending, following example from
-     https://sourceforge.net/apps/trac/sourceforge/wiki/Project%20Web%20Email%20Configuration
-     I want to make sure that I have working config, I need email working for my local piwik
-     (as piwik hosted by SF is broken), and probably wordpress in the future.
+/* Testing mail sending, following example from
+   https://sourceforge.net/p/forge/documentation/Project%20Web%20Email/ .
+   We want to make sure that we have working config, for Wordpress.
 
-     Needed local install of pear package Mail.php
+   Needed local install of pear package Mail.php
 
-  // pear config-set php_dir /home/project-web/castle-engine/pear
-  // pear config-set data_dir /home/project-web/castle-engine/pear
-  // pear config-set test_dir /home/project-web/castle-engine/pear/test
-  // pear config-set doc_dir /home/project-web/castle-engine/pear/doc
-  // pear install --alldeps mail
-  */
+// pear config-set php_dir /home/project-web/castle-engine/pear
+// pear config-set data_dir /home/project-web/castle-engine/pear
+// pear config-set test_dir /home/project-web/castle-engine/pear/test
+// pear config-set doc_dir /home/project-web/castle-engine/pear/doc
+// check: less ~/.pearrc
+// pear install --alldeps mail
+
+*/
 
 set_include_path('/home/project-web/castle-engine/pear' . PATH_SEPARATOR
                  . get_include_path());
@@ -27,7 +28,7 @@ include('Mail.php');
 $recipients = array( 'michalis.kambi@gmail.com' ); # Can be one or more emails
 
 $headers = array (
-		  'From' => 'admin@castle-engine.sourceforge.net',
+		  'From' => 'no-reply@castle-engine.sourceforge.io',
 		  'To' => join(', ', $recipients),
 		  'Subject' => 'Testing email from project web',
 		  );
@@ -35,12 +36,12 @@ $headers = array (
 $body = "This was sent via php from project web!\n";
 
 $mail_object =& Mail::factory('smtp',
-			      array(
-				    'host' => 'prwebmail',
-				    'auth' => true,
-				    'username' => 'castle-engine',
-				    'password' => 'FILL-THIS', # As set on your project's config page
-        'debug' => true, # uncomment to enable debugging
+    array(
+         'host' => 'prwebmail',
+         'auth' => true,
+         'username' => 'castle-engine',
+         'password' => 'FILL-THIS', # As set on your project's config page
+         'debug' => true, # uncomment to enable debugging
     ));
 
 echo 'Start...' . "\n";
