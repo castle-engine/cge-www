8d9569a64790e2b48e2759981db51ad4d93cf2fb
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-02-21 06:42:19 +0100

    Make news_feed.php go to Wordpress RSS

--- htdocs/news_feed.php
+++ htdocs/news_feed.php
@@ -1,4 +1,28 @@
-<?php header('Location: http://gplusrss.com/rss/feed/c3dd3cd5ec6725d92b8b5b0633b2daa852f29730ecc6b'); ?>
+<?php
+
+/* This link is used by http://planetdev.freegamedev.net/ , so keep it working.
+
+   We could notify them to change link to Wordpress, see
+   http://forum.freegamedev.net/viewtopic.php?f=24&t=7209
+   http://forum.freegamedev.net/viewtopic.php?f=24&t=24
+   Actually, we could submit a pull request:
+   https://github.com/FreeGameDev/planet-config
+   https://github.com/FreeGameDev/planet-config/blob/master/fgdplanet-dev-feeds.ini
+
+   But it's more future-proof for us to just change this page to redirect
+   wherever necessary. */
+
+require_once 'castle_engine_functions.php';
+header('Location: ' . CURRENT_URL . 'wp/feed/');
+?>
+
+<?php
+/*
+This is (sometimes?) failing with internal error since around 2017-02-20.
+Also this is not useful anymore, as we now have our own feed made by Wordpress.
+header('Location: http://gplusrss.com/rss/feed/c3dd3cd5ec6725d92b8b5b0633b2daa852f29730ecc6b');
+*/
+?>
 
 <?php
 /*
