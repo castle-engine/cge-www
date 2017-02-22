7d8ee5c9ad1fd1d05661f39ef6ca2fbd576e982c
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-02-22 19:05:13 +0100

    Update secure permissions script - harden permissions

--- scripts/shell.sf.net/secure_permissions.sh
+++ scripts/shell.sf.net/secure_permissions.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 set -eu
 
-securefile ()
-{
-  ls -Flah "$1"
-  chmod o-rwX "$1"
-  ls -Flah "$1"
-}
-
 PROJ=/home/project-web/castle-engine/
 
+# others cannot do anything
+find "$PROJ" -exec chmod 'o-rwX' '{}' ';'
+
+# no need for this anymore
+# securefile ()
+# {
+#   ls -Flah "$1"
+#   chmod o-rwX "$1"
+#   ls -Flah "$1"
+# }
 # securefile "$PROJ"cge-www/htdocs/wp/wp-config-production.php
 # securefile "$PROJ"cge-www/sourceforge_tests/test_out_mail.php
 
-# others cannot do anything
-find "$PROJ"htdocs \
-  -type f -exec chmod 'o-rwX' '{}' ';'
-find "$PROJ"htdocs \
-  -type d -exec chmod 'o-rwX' '{}' ';'
-
+# make sure some Wordpress dirs are writeable by the apache group
 mkdir -p \
   "$PROJ"cge-www/htdocs/wp/wp-content/cache/ \
   "$PROJ"cge-www/htdocs/wp/wp-content/uploads/
-
 chmod -R g+w \
   "$PROJ"cge-www/htdocs/wp/wp-content/cache/ \
   "$PROJ"cge-www/htdocs/wp/wp-content/uploads/
