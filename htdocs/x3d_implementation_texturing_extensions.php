680011391aec155abb2d4f98cb805ebbe5304eb4
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-03-16 18:26:04 +0100

    Make embedded youtube work over https

--- htdocs/x3d_implementation_texturing_extensions.php
+++ htdocs/x3d_implementation_texturing_extensions.php
@@ -373,7 +373,7 @@ and rendering, regardless of initial texture contents.
     array('html' =>
       '<div class="thumbs_cell_with_text_or_movie">This movie shows how it looks animated.'
       . (!HTML_VALIDATION ?
-      '<object class="youtube_thumbnail_video"><param name="movie" value="http://www.youtube.com/v/6ecZInTrfak"> </param> <embed src="http://www.youtube.com/v/6ecZInTrfak" type="application/x-shockwave-flash" width="200" height="167"> </embed> </object>'
+      '<object class="youtube_thumbnail_video"><param name="movie" value="https://www.youtube.com/v/6ecZInTrfak"> </param> <embed src="https://www.youtube.com/v/6ecZInTrfak" type="application/x-shockwave-flash" width="200" height="167"> </embed> </object>'
       : '')
       . '</div>'),
   ));
