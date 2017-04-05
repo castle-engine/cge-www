818d18374fd8445724745af28ff9c86b05492d93
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-04-05 17:05:29 +0200

    latest.php.zip to redirect to latest engine zip

---
+++ htdocs/latest.zip.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'castle_engine_functions.php';
+header('Location: https://downloads.sourceforge.net/' . SF_UNIX_NAME . '/castle_game_engine-' . VERSION_CASTLE_GAME_ENGINE . '-src.zip');
+?>
