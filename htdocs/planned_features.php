f0591d5409523da10b10e2bd54d483be5e22b3e4
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-12-30 01:49:26 +0100

    iOS improvements are done now

--- htdocs/planned_features.php
+++ htdocs/planned_features.php
@@ -383,20 +383,6 @@ fade_duration="min(animation_duration * 0.25, target_animation_duration * 0.25, 
 
   <li>
     <p><b>Easy way to use 3rd-person camera movement in your games</b>
-
-  <li><p><b>iOS improvements:</b>
-
-    <ul>
-      <li><p>(IN PROGRESS) <i>Services on iOS</i>.
-        For now, most of <a href="https://github.com/castle-engine/castle-engine/wiki/Android-Project-Services-Integrated-with-Castle-Game-Engine">our services</a> are only on Android.
-        Most of them are possible also on iOS, e.g.
-        <?php api_link('TInAppPurchases', 'CastleInAppPurchases.TInAppPurchases.html'); ?>,
-        or <i>Apple Game Center</i> as an equivalent to <i>Google Play Games</i> from Android.
-
-      <li><p>(DONE) <i>Package the vorbisfile library</i>. To be able to read OggVorbis sound files.
-
-      <li><p><i>Enable texture compression</i>. Because of Apple's weird implementation of PVRTC format (only square textures are allowed), for now we disabled texture compression on iOS. This should be done better, at least to enable the texture compression of other formats.
-    </ul>
 </ul>
 
 <?php castle_footer(); ?>
