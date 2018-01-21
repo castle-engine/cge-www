3c2374b738fbe4d84570fac78777c16296de27bc
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-01-21 12:07:46 +0100

    Updated version of everything for next release

--- htdocs/generated_versions.php
+++ htdocs/generated_versions.php
@@ -3,14 +3,14 @@
   define('VERSION_LETS_TAKE_A_WALK', '1.2.4');
   define('VERSION_MALFUNCTION', '1.2.8');
   define('VERSION_KAMBI_LINES', '1.1.7');
-  define('VERSION_VIEW3DSCENE', '3.17.0');
+  define('VERSION_VIEW3DSCENE', '3.18.0');
   define('VERSION_RAYHUNTER', '1.3.4');
-  define('VERSION_GLVIEWIMAGE', '1.7.0');
+  define('VERSION_GLVIEWIMAGE', '1.8.0');
   define('VERSION_GLPLOTTER', '1.2.5');
   define('VERSION_GLINFORMATION', '1.2.2');
   define('VERSION_GEN_FUNCTION', '1.0.5');
   define('VERSION_MOUNTAINS_OF_FIRE', '1.1.0');
   define('VERSION_GLINFORMATION_GLUT', '1.2.2');
-  define('VERSION_CASTLE_GAME_ENGINE', '6.2');
-  define('VERSION_DEMO_MODELS', '3.8.0');
+  define('VERSION_CASTLE_GAME_ENGINE', '6.4');
+  define('VERSION_DEMO_MODELS', '3.9.0');
 ?>
