6f71518078ce4c5b52e388d294dda6428153ef11
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-01-23 04:46:18 +0100

    Release on itch.io with manifests

--- pack/upload_itch_io.sh
+++ pack/upload_itch_io.sh
@@ -13,37 +13,54 @@ FILE_RELEASES=~/castle-engine-release/file_releases
 
 . generated_versions.sh
 
-# butler prefers unpacked files.
-# Otherwise, it will treat zip differently (will unpack it before uploading)
-# than tar.gz (will treat it as single file, internally: directory with 1 file).
-# See https://itch.io/docs/butler/single-files.html
+# We unpack the archive to temporary directory.
 #
-# So we just unpack tar.gz before passing contents to butler.
-temporary_unpack_tar_gz ()
+# - Becase butler prefers unpacked files.
+#   Otherwise, it will treat zip differently (will unpack it before uploading)
+#   than tar.gz (will treat it as single file, internally: directory with 1 file).
+#   See https://itch.io/docs/butler/single-files.html
+#
+# - We also unpack, to place .itch.toml file inside, during butler_push.
+temporary_unpack ()
 {
   UPLOAD_DIR=/tmp/upload_itch_io_unpacked_$$
   rm -Rf "$UPLOAD_DIR"
   mkdir "$UPLOAD_DIR"
-  cp "${FILE_RELEASES}"/view3dscene-"${VER}"-linux-x86_64.tar.gz "$UPLOAD_DIR"/temp.tar.gz
 
-  pushd .
-  cd "$UPLOAD_DIR"
-  tar xzvf temp.tar.gz
-  rm -f temp.tar.gz
-  popd
+  local FILE_EXTENSION=".${FILE_NAME##*.}"
+
+  case "$FILE_EXTENSION" in
+    .zip)
+      cp "${FILE_NAME}" "$UPLOAD_DIR"/temp.zip
+      pushd .
+      cd "$UPLOAD_DIR"
+      unzip temp.zip
+      rm -f temp.zip
+      popd
+      ;;
+    .gz) # assuming .tar.gz
+      cp "${FILE_NAME}" "$UPLOAD_DIR"/temp.tar.gz
+      pushd .
+      cd "$UPLOAD_DIR"
+      tar xzvf temp.tar.gz
+      rm -f temp.tar.gz
+      popd
+      ;;
+    *)
+      echo "Cannot deal with file $FILE_NAME, extension $FILE_EXTENSION"
+      exit 1
+      ;;
+  esac
 }
 
-butler_push_zip ()
+butler_push ()
 {
-  butler push "$@"
-}
+  MANIFEST=itch_io_manifests/"$1"
+  FILE_NAME="$2"
+  shift 2
 
-butler_push_tar_gz ()
-{
-  FILE_NAME="$1"
-  shift 1
-
-  temporary_unpack_tar_gz "FILE_NAME"
+  temporary_unpack "FILE_NAME"
+  cp -f "${MANIFEST}" "$UPLOAD_DIR"/.itch.toml
   butler push "$UPLOAD_DIR" "$@"
   rm -Rf "$UPLOAD_DIR"
 }
@@ -51,17 +68,17 @@ butler_push_tar_gz ()
 do_view3dscene ()
 {
   VER="${GENERATED_VERSION_VIEW3DSCENE}"
-  butler_push_zip    "${FILE_RELEASES}"/view3dscene-"${VER}"-win-i386.zip        michaliskambi/view3dscene:windows --userversion "${VER}"
-  butler_push_tar_gz "${FILE_RELEASES}"/view3dscene-"${VER}"-linux-x86_64.tar.gz michaliskambi/view3dscene:linux   --userversion "${VER}"
+  butler_push view3dscene-windows.itch.toml "${FILE_RELEASES}"/view3dscene-"${VER}"-win-i386.zip        michaliskambi/view3dscene:windows --userversion "${VER}"
+  butler_push view3dscene-linux.itch.toml "${FILE_RELEASES}"/view3dscene-"${VER}"-linux-x86_64.tar.gz michaliskambi/view3dscene:linux   --userversion "${VER}"
   echo 'Run "butler status michaliskambi/view3dscene" to watch'
 }
 
 do_castle_game_engine ()
 {
   VER="${GENERATED_VERSION_CASTLE_GAME_ENGINE}"
-  butler_push_zip "${FILE_RELEASES}"/castle_game_engine-"${VER}"-src.zip michaliskambi/castle-game-engine:source-code --userversion "${VER}"
-  echo 'Run "butler status michaliskambi/castle_game_engine" to watch'
+  butler_push castle_game_engine.itch.toml "${FILE_RELEASES}"/castle_game_engine-"${VER}"-src.zip michaliskambi/castle-game-engine:source-code --userversion "${VER}"
+  echo 'Run "butler status michaliskambi/castle-game-engine" to watch'
 }
 
 do_view3dscene
-#do_castle_game_engine
+do_castle_game_engine
