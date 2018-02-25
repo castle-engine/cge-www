70af11f13b537f089faf76785849fec20983f6fe
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-02-25 20:41:13 +0100

    Update Google Analytics code

--- htdocs/castle_engine_externals.php
+++ htdocs/castle_engine_externals.php
@@ -140,15 +140,14 @@ function echo_piwik_tracking()
 function echo_google_analytics_tracking()
 {
 ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-61279460-1"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  ga('create', 'UA-61279460-1', 'auto');
-  ga('send', 'pageview');
-
+  gtag('config', 'UA-61279460-1');
 </script>
 <?php
 }
