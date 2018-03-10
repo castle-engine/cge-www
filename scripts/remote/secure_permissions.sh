b6b752998da8f4d61d91e2fb90ac45508d2a4071
Michalis Kamburelis <michalis.kambi@gmail.com>
2018-03-10 17:48:36 +0000

    permissions to easier upgrade

--- scripts/remote/secure_permissions.sh
+++ scripts/remote/secure_permissions.sh
@@ -7,18 +7,21 @@ secure_dir ()
   chmod -R u=rwX,g=rwX,o=rX  "$1"
   chown -R michalis:michalis "$1"
 }
-secure_dir ~/cge-html/
-secure_dir ~/cge-www/
+secure_dir /home/michalis/cge-www/
+secure_dir /home/michalis/cge-html/
 
 # Make sure some dirs writeable
 writeable_dir ()
 {
   mkdir -p "$1"
-  chmod -R o+w "$1"
+  # We make directory writeable by making it group-owned by www-data,
+  # and group already has write permissions (set by secure_dir).
+  # This way:
+  # - we do not open this directory for anyone on server
+  # - we do not need sudo to call this (unlike chown)
+  chgrp -R www-data "$1"
 }
-writeable_dir ~/cge-www/htdocs/wp/wp-content/cache/
-writeable_dir ~/cge-www/htdocs/wp/wp-content/uploads/
-
-# Piwik owned by www-data, as www-data upgrades it, creates files there etc.
-sudo chown -R www-data:www-data ~/cge-html-piwik/
-sudo chmod -R u=rwX,g=rwX,o=rX  ~/cge-html-piwik/
+# writeable_dir /home/michalis/cge-www/htdocs/wp/wp-content/cache/
+# writeable_dir /home/michalis/cge-www/htdocs/wp/wp-content/uploads/
+# Eventually make whole Wordpress writeable, to allow upgrading through www easily
+writeable_dir /home/michalis/cge-www/htdocs/wp/
