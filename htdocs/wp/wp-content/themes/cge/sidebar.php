69a49e8dc02e405ac72858dfb868036eb6b99610
Michalis Kamburelis <michalis.kambi@gmail.com>
2019-02-16 21:14:41 +0100

    More Google+ removal. Move Mastodon and Patreon up.

--- htdocs/wp/wp-content/themes/cge/sidebar.php
+++ htdocs/wp/wp-content/themes/cge/sidebar.php
@@ -14,10 +14,10 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
     <div class="panel-follow-us">
         Follow us to get the latest news about the engine development:
         <ul>
+          <li><a href="https://www.patreon.com/castleengine">Patreon</a>
+          <li><a href="https://mastodon.social/@michalis_kambi">Mastodon</a>
           <li><a href="https://www.facebook.com/castleengine">Facebook</a>
           <li><a href="https://twitter.com/castleengine">Twitter</a>
-          <li><a href="https://plus.google.com/+CastleGameEngineX3d">Google+</a>
-          <li><a href="https://mastodon.social/@michalis_kambi">Mastodon</a>
         </ul>
     </div>
 
