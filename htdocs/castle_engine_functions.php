1596845dd8c53addb4683f3dcbae30a4b40c69f8
Michalis Kamburelis <michalis.kambi@gmail.com>
2020-07-02 20:48:21 +0200

    Improve cross-platform notes

--- htdocs/castle_engine_functions.php
+++ htdocs/castle_engine_functions.php
@@ -255,7 +255,7 @@ $castle_sitemap = array(
           'manual_quick_2d_game' => array('title' => 'Quick 2D game (basic window events)'),
           'manual_load_3d' => array('title' => 'Loading, displaying a scene'),
           'manual_scene' => array('title' => 'Transform, animate, duplicate, build a scene'),
-          'manual_cross_platform' => array('title' => 'Cross-platform (desktop, mobile and more) games',
+          'manual_cross_platform' => array('title' => 'Cross-platform (desktop, mobile, consoles...) games',
             'sub' => array(
               'manual_data_directory' => array('title' => 'Data directory'),
               'manual_build_tool' => array('title' => 'Build Tool (wiki)', 'url' => 'https://github.com/castle-engine/castle-engine/wiki/Build-Tool'),
