716fd55b0cfce4cf7c5daf03a341004a708100d3
Michalis Kamburelis <michalis.kambi@gmail.com>
2020-11-27 02:04:59 +0100

    Use list-no-margin better

--- htdocs/castle-engine-website-base/kambi_toc.php
+++ htdocs/castle-engine-website-base/kambi_toc.php
@@ -124,7 +124,12 @@ class TableOfContents {
 
       if ($now_nesting == $old_nesting + 1)
       {
-        $result .= "<ol class=\"list-no-margin\">\n" . $list_item;
+        /* Only nested lists use list-no-margin.
+           Testcase: ttps://castle-engine.io/creating_data_blender.php ,
+           should have space between ToC and 1st paragraph,
+           but no extra space inside ToC. */
+        $classes = $now_nesting == 0 ? '' : 'list-no-margin';
+        $result .= "<ol class=\"" . $classes . "\">\n" . $list_item;
         $this->push_last_number($current_number, 1);
       } else
       if ($now_nesting == $old_nesting)
