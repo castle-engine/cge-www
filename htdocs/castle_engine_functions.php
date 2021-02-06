11cf62a3de00f4fc4021f725d2a90a9b88859e6f
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-02-06 02:45:30 +0100

    Remove unused CGE_LATEST_STABLE_DOWNLOAD

--- htdocs/castle_engine_functions.php
+++ htdocs/castle_engine_functions.php
@@ -111,7 +111,6 @@ define('FORUM_URL',           'https://forum.castle-engine.io/');
 define('PATREON_URL',         'https://patreon.com/castleengine');
 
 define('CGE_LATEST_DOWNLOAD', 'https://github.com/castle-engine/castle-engine/archive/snapshot.zip');
-// unused: define('CGE_LATEST_STABLE_DOWNLOAD', 'https://github.com/castle-engine/castle-engine/releases/download/v6.4/castle_game_engine-6.4-src.zip');
 define('CGE_LATEST_UNSTABLE_DOWNLOAD', 'https://github.com/castle-engine/castle-engine/releases/tag/snapshot');
 
 function reference_link()
