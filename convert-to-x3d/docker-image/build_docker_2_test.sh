b807cc8f867ae8305a999f74998d766636013a3d
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-05-09 03:10:55 +0200

    Remove debug output

--- convert-to-x3d/docker-image/build_docker_2_test.sh
+++ convert-to-x3d/docker-image/build_docker_2_test.sh
@@ -3,6 +3,5 @@ set -euo pipefail
 
 # Run tests on convert-to-x3d Docker image.
 DOCKER_TEST="docker run --name test-convert-to-x3d --rm convert-to-x3d"
-$DOCKER_TEST ls -Flah /usr/local/bin/
 $DOCKER_TEST tovrmlx3d --version
 $DOCKER_TEST view3dscene --version
