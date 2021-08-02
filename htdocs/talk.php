b19f085c891e0ec5e707b1c1ab3708d2e9158c27
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-08-02 00:25:25 +0000

    Reformat talk page to more prominently link to forum, github issues, patreon

--- htdocs/talk.php
+++ htdocs/talk.php
@@ -4,35 +4,44 @@ require_once 'castle_engine_functions.php';
 castle_header('Talk with us');
 ?>
 
-<div class="row">
-    <div class="col-sm-6" style="text-align: right">
-        <iframe style="inline-block" src="https://discordapp.com/widget?id=389676745957310465&amp;theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+<div class="fixed-width-content">
+
+  <div class="talk-buttons-row">
+    <iframe class="talk-button" src="https://discordapp.com/widget?id=389676745957310465&amp;theme=dark" allowtransparency="true" frameborder="0"></iframe>
+    <a href="<?php echo FORUM_URL; ?>" class="talk-button btn btn-info btn-lg">
+      <p>
+        <img src="<?php echo CURRENT_URL; ?>/images/discourse_icon.png" alt="Discourse Forum"><br>
+        Forum
+      </p>
+    </a>
+    <a href="https://github.com/castle-engine/castle-engine/issues" class="talk-button btn btn-info btn-lg">
+      <p>
+        <img src="<?php echo CURRENT_URL; ?>/images/os_icons/github.png" alt="GitHub Issues"><br>
+        Submit GitHub issue
+      </p>
+    </a>
+    <a href="https://www.patreon.com/castleengine" class="talk-button btn btn-success btn-lg">
+      <p>
+        <img src="<?php echo CURRENT_URL; ?>/images/patreon-brand/logo/original-png/Digital-Patreon-Logo_White.png" alt="Patreon"><br>
+        Support on Patreon
+      </p>
+    </a>
+    <!--
+    Too much info
+    <div class="talk-button">
+      <p>Social:
+      <ul>
+        <li><a href="https://www.facebook.com/castleengine">Facebook</a></li>
+        <li><a href="https://twitter.com/castleengine">Twitter</a></li>
+        <li><a href="https://mastodon.social/@michalis_kambi">Mastodon</a></li>
+      </ul>
     </div>
-    <div class="col-sm-6">
-        <p><a href="<?php echo FORUM_URL; ?>" class="btn btn-default btn-lg">Post on our forum</a>
+    <a href="helping.php" class="talk-button btn btn-info btn-lg">
+      <p>Many ways to help with engine development</p>
+    </a>
+    -->
+  </div>
 
-        <p>You can also
-
-        <ul>
-          <li><a href="https://github.com/castle-engine/castle-engine/issues">submit Castle Game Engine bugs or pull requests</a>,</li>
-          <li>or <a href="https://github.com/castle-engine/view3dscene/issues">submit view3dscene bugs or pull requests</a></li>
-        </ul>
-
-        <p>Other ways to contact us:
-
-        <ul>
-          <li><a href="https://www.patreon.com/castleengine">Talk and support us on Patreon</a></li>
-          <li><a href="wp/">Comment on our blog</a>
-          <li>Follow and talk to us on:
-            <ul>
-              <li><a href="https://www.facebook.com/castleengine">Facebook</a></li>
-              <li><a href="https://twitter.com/castleengine">Twitter</a></li>
-              <li><a href="https://mastodon.social/@michalis_kambi">Mastodon</a></li>
-            </ul>
-        </ul>
-
-        <p>If you're looking for ways to contribute, <a href="helping.php">there's a number of tasks (for developers and non-developers) how you can help!</a>
-    </div>
 </div>
 
 <?php
