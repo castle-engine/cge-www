83eda7134d783d673c190e976399814e95bfc67f
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-08-02 04:30:21 +0200

    Simplify wording, no judge good/high quality, this is already said at top

--- htdocs/assets.php
+++ htdocs/assets.php
@@ -11,32 +11,32 @@ echo pretty_heading($page_title);
 
 <div class="row">
   <?php gallery_link('OpenGameArt.org',
-    'Good quality art (3D, 2D, sounds) uploaded by various artsts on clear open-source licenses.
+    'Art (3D, 2D, sounds) uploaded by various artsts on clear open-source licenses.
     <p><i>Hint:</i> Do you want to share an asset you made and tested specifically for CGE game? Upload it to <a href="https://opengameart.org/art-search-advanced?keys=&title=%5Bcge%5D">OpenGameArt.org with <code>[CGE]</code> in the name</a>.',
     'assets_opengameart.png',
     'https://opengameart.org/');
   ?>
 
   <?php gallery_link('Kenney',
-    'Lots of high quality art (sprites, 3D models, sound effects) on a public domain ("do what you want") license.',
+    'Sprites, 3D models, sound effects on a public domain ("do what you want") license.',
     'assets_kenney.png',
     'https://kenney.nl/');
   ?>
 
   <?php gallery_link('Sketchfab glTF models',
-    'Good quality 3D models, in glTF format, uploaded by various arists. Some free (on clear licenses, like <i>Creative Commons</i>), some paid.',
+    '3D models, in glTF format, uploaded by various arists. Some free (on clear licenses, like <i>Creative Commons</i>), some paid.',
     'assets_sketchfab.png',
     'https://sketchfab.com/features/gltf');
   ?>
 
   <?php gallery_link('Blend Swap',
-    'Good quality 3D models in <a href="https://www.blender.org/">Blender</a> (which you can <a href="creating_data_blender.php">export to glTF easily</a>). Uploaded by various artists. All on <i>Creative Commons</i> licenses.',
+    '3D models in <a href="https://www.blender.org/">Blender</a> (which you can <a href="creating_data_blender.php">export to glTF easily</a>). Uploaded by various artists. All on <i>Creative Commons</i> licenses.',
     'assets_blendswap.png',
     'https://blendswap.com/');
   ?>
 
   <?php gallery_link('Quaternius',
-    'Lots of good quality 3D model packs, on public domain license.', // https://www.patreon.com/quaternius
+    'Many 3D model packs, on a public domain license.', // https://www.patreon.com/quaternius
     'assets_quaternius.png',
     'https://quaternius.com/');
   ?>
