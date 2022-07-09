35b2d85d9e5831a73589d97587351a6f0f6e57f5
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-07-09 00:08:34 +0000

    Wordpress core and plugin upgrades

--- htdocs/wp/wp-content/plugins/jetpack/jetpack_vendor/automattic/jetpack-identity-crisis/build/index.asset.php
+++ htdocs/wp/wp-content/plugins/jetpack/jetpack_vendor/automattic/jetpack-identity-crisis/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '4a6e6592e7a0125664c9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'fc29c2a9fa7082beb39c');
