efc4488b8b5d2feff2f176adbb879808ad7e76f0
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-07-15 06:01:35 +0200

    Read description and og:image from AsciiDoctor documents, for now test on Blender

--- htdocs/doc.php
+++ htdocs/doc.php
@@ -60,22 +60,72 @@ $page_basename = 'doc/' . $page_name;
 
 $adoc_file = 'doc/' . $page_name . '.adoc';
 
-/* Read title, from AsciiDoctor first line */
-$adoc_file_handle = @fopen($adoc_file, 'r');
-if ($adoc_file_handle === FALSE) {
-  castle_fail_404('Cannot open page: ' . $_GET['page'] . '.');
-}
-$adoc_first_line = fgets($adoc_file_handle);
-fclose($adoc_file_handle);
-if (is_prefix('# ', $adoc_first_line)) {
-  $title = remove_prefix('# ', $adoc_first_line);
-} else {
-  $title = $page_name; // use internal page name as a fallback title
+/* Read .adoc file and extract some informatio from header.
+
+   Returns associative array with possible values (but nothing is guaranteed):
+   - 'title'
+   - 'description' (suitable for meta_description parameter to castle_header)
+   - 'social_share_image' (suitable for social_share_image parameter to castle_header)
+*/
+function castle_read_adoc_header($adoc_file)
+{
+  $result = array();
+
+  $adoc_file_handle = @fopen($adoc_file, 'r');
+  if ($adoc_file_handle === FALSE) {
+    castle_fail_404('Cannot open page: ' . $_GET['page'] . '.');
+  }
+
+  $adoc_first_line = fgets($adoc_file_handle);
+  if (is_prefix('# ', $adoc_first_line)) {
+    $result['title'] = trim(remove_prefix('# ', $adoc_first_line));
+  }
+
+  for ($i = 1; $i <= 5; $i++) { // read a few first lines to scan for metadata
+    $adoc_header_line = fgets($adoc_file_handle);
+    if ($adoc_header_line === FALSE) {
+      break;
+    }
+    /* Interpreting AsciiDoc :description: as HTML meta description follows AsciiDoctor
+       standard, see https://docs.asciidoctor.org/asciidoc/latest/document/metadata/ .
+       We just do it manually here, because we generate header on CGE side.
+
+       Note that we use trim, as fgets result contains newline which we don't want.
+    */
+    if (is_prefix(':description: ', $adoc_header_line)) {
+      $result['description'] = trim(remove_prefix(':description: ', $adoc_header_line));
+    }
+    if (is_prefix(':cge-social-share-image: ', $adoc_header_line)) {
+      $result['social_share_image'] = trim(remove_prefix(':cge-social-share-image: ', $adoc_header_line));
+    }
+  }
+
+  fclose($adoc_file_handle);
+
+  return $result;
 }
 
-castle_header($title, array(
+$adoc_header = castle_read_adoc_header($adoc_file);
+
+// calculate $title
+if (empty($adoc_header['title'])) {
+  $title = $page_name; // use internal page name as a fallback title
+} else {
+  $title = $adoc_header['title'];
+}
+
+// calculate $header_params and call castle_header
+$header_params = array(
   'canonical_url' => CASTLE_PROD_URL . $page_name
-));
+);
+// Pass information from $adoc_header to $header_params
+if (!empty($adoc_header['description'])) {
+  $header_params['meta_description'] = $adoc_header['description'];
+}
+if (!empty($adoc_header['social_share_image'])) {
+  $header_params['social_share_image'] = $adoc_header['social_share_image'];
+}
+castle_header($title, $header_params);
 
 global $castle_current_book;
 if ($castle_current_book == NULL) { // in case of book, pretty_heading is already done by castle_header
