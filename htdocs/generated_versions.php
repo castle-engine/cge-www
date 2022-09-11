7953eb8205c66fea9a211a4b53cc5c98ef4d9885
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-09-11 22:04:27 +0200

    Update versions for next release

--- htdocs/generated_versions.php
+++ htdocs/generated_versions.php
@@ -2,10 +2,10 @@
   define('VERSION_CASTLE', '1.0.1');
   define('VERSION_MALFUNCTION', '1.2.8');
   define('VERSION_KAMBI_LINES', '1.1.7');
-  define('VERSION_VIEW3DSCENE', '4.0.0');
+  define('VERSION_VIEW3DSCENE', '4.2.0');
   define('VERSION_RAYHUNTER', '1.3.4');
   define('VERSION_CASTLE_VIEW_IMAGE', '2.0.0');
   define('VERSION_GLPLOTTER', '2.0.0');
   define('VERSION_MOUNTAINS_OF_FIRE', '1.1.0');
-  define('VERSION_CASTLE_GAME_ENGINE', '7.0-alpha.1');
+  define('VERSION_CASTLE_GAME_ENGINE', '7.0-alpha.2');
 ?>
