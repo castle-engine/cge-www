94555c0d7facddecd894f4696f877110408e4dd2
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-10-30 20:58:46 +0000

    Wordpress core and plugin upgrades

---
+++ htdocs/wp/wp-content/plugins/jetpack-protect/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c28c9ff5fed1d1b9867f');
