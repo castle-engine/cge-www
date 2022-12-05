aae05306627bcce254a6bf7a0fd18d45f33eacd9
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-12-05 02:06:15 +0100

    Test jQuery hack for passive event listeners

--- htdocs/castle-engine-website-base/castle-engine.js
+++ htdocs/castle-engine-website-base/castle-engine.js
@@ -28,3 +28,31 @@ jQuery(document).ready(function() {
 jQuery("#toggle-details").click(function() {
   jQuery("#details").toggle(50);
 });
+
+/* Using passive event listeners can improve scrolling performance, according to
+   https://web.dev/uses-passive-event-listeners/
+   (linked from Lighthouse).
+   Solution for jQuery from
+   https://stackoverflow.com/questions/60357083/does-not-use-passive-listeners-to-improve-scrolling-performance-lighthouse-repo
+*/
+
+jQuery.event.special.touchstart = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+};
+jQuery.event.special.touchmove = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+};
+jQuery.event.special.wheel = {
+    setup: function( _, ns, handle ){
+        this.addEventListener("wheel", handle, { passive: true });
+    }
+};
+jQuery.event.special.mousewheel = {
+    setup: function( _, ns, handle ){
+        this.addEventListener("mousewheel", handle, { passive: true });
+    }
+};
