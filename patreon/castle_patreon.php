3006de775a0e80419e2747dbd31ad3911b5f94d2
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-01-24 10:51:11 +0100

    Update Patreon

--- patreon/castle_patreon.php
+++ patreon/castle_patreon.php
@@ -180,7 +180,7 @@ if (QUERY_PATREON_API) {
   /* Workaround: just hardcoded and manually updated amount. */
   function castle_patreon_total_pledges()
   {
-    return 24400;
+    return 24800;
   }
 
 }
