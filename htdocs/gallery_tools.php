bab1d8a0a7764aa8aa040242f1150406ab347e9a
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-04-18 03:42:18 +0200

    Improve tool descriptions, add castle-model-converter box and screenshot

--- htdocs/gallery_tools.php
+++ htdocs/gallery_tools.php
@@ -12,22 +12,26 @@ echo pretty_heading($page_title);
 We love to see how you use CGE.</i>
 
 <div class="row">
-  <?php gallery_link('view3dscene',
-    'VRML / X3D browser, and a viewer for other 3D model formats
-    (Collada, 3DS, MD3, Wavefront OBJ, Spine...).
+  <?php gallery_link('Castle Model Viewer (formerly view3dscene)',
+    'Viewer for 3D and 2D models like glTF, X3D, VRML, Collada, 3DS, MD3, Wavefront OBJ, Spine and <a href="creating_data_model_formats.php">many others supported by Castle Game Engine</a>.
     Explore the virtual world, with collision-checking, gravity, interactive animations, shadows, mirrors, shaders and more.
-    Convert various models to VRML/X3D.',
+    Can also convert various models to X3D.',
     'view3dscene_outlines.png',
-    'view3dscene'); ?>
+    'doc/castle-model-viewer'); ?>
 
-  <?php gallery_link('Convert everything to X3D',
-    'Convert onine glTF, OBJ, STL, Collada, 3DS (and other model formats handled by Castle Game Engine) to X3D. This is an online version of our <a href="view3dscene.php"><code>tovrmlx3d</code> converter (distributed with view3dscene)</a>.',
+<?php gallery_link('Castle Model Converter (formerly tovrmlx3d)',
+    'Command-line converter from <a href="creating_data_model_formats.php">all model formats supported by Castle Game Engine (like glTF)</a> to X3D. Can also act as validator.',
+    'castle-model-converter.png',
+    'doc/castle-model-converter'); ?>
+
+  <?php gallery_link('Online Model Converter',
+    'Convert 3D and 2D models. Can convert glTF, OBJ, STL, Collada, 3DS (and other model formats handled by Castle Game Engine) to X3D. This is an online version of our <a href="model-converter">Castle Model Converter</a>.',
     'online-model-converter.png',
     'convert'); ?>
 
-  <?php gallery_link('castle-view-image (formerly glViewImage)',
-    'Image viewer, handles many image formats (including some exotic ones: DDS, RGBE).',
-    "castle-view-image_dds.png", 'castle-view-image'); ?>
+  <?php gallery_link('Castle Image Viewer',
+    'Image viewer, handles many common image formats (PNG, JPG...) and some special formats used in game development (DDS, KTX, RGBE)',
+    "castle-view-image_dds.png", 'doc/castle-image-viewer'); ?>
 
   <?php gallery_link("Curves Tool",
     "Design a 2D curve, save it as XML, and use it in your own programs, e.g. to move something (camera, another object) along a smooth designed trajectory.",
