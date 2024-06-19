2f68cfb1c386f2c20704063e18391c9b7e222ffd
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-06-19 10:40:38 +0200

    Set executable bit when copying

--- online-model-converter/docker-image/build_docker_1_build.sh
+++ online-model-converter/docker-image/build_docker_1_build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+# We use "COPY --chmod...", which requires DOCKER_BUILDKIT.
+# See https://stackoverflow.com/questions/56558570/copying-files-with-execute-permissions-in-docker-image
+export DOCKER_BUILDKIT=1
+
 docker build -t online-model-converter -f Dockerfile docker-context/
