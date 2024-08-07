0c1f2cc13bc0e597b6774a62ec60ede386e2415f
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-08-07 22:58:07 +0000

    Wordpress core and plugin upgrades

---
+++ htdocs/wp/wp-content/jetpack-waf/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+define( 'DISABLE_JETPACK_WAF', false );
+if ( defined( 'DISABLE_JETPACK_WAF' ) && DISABLE_JETPACK_WAF ) return;
+define( 'JETPACK_WAF_MODE', 'silent' );
+define( 'JETPACK_WAF_SHARE_DATA', false );
+define( 'JETPACK_WAF_SHARE_DEBUG_DATA', false );
+define( 'JETPACK_WAF_DIR', '/home/michalis/cge-www/htdocs/wp/wp-content/jetpack-waf' );
+define( 'JETPACK_WAF_WPCONFIG', '/home/michalis/cge-www/htdocs/wp/wp-content/../wp-config.php' );
+require_once '/home/michalis/cge-www/htdocs/wp/wp-content/plugins/jetpack/vendor/autoload.php';
+Automattic\Jetpack\Waf\Waf_Runner::initialize();
