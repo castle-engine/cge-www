a179e029e43d0272613f5996ce6eeaade1b53fed
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-08-23 06:19:30 +0200

    Upload to itch.io: download using GH CLI, upload also bundled, upload also macOS

--- pack/upload_itch_io.sh
+++ pack/upload_itch_io.sh
@@ -5,10 +5,13 @@ IFS=$'\n\t'
 # -----------------------------------------------------------------------
 # This script uploads Castle Game Engine packages
 # to https://castle-engine.itch.io/castle-game-engine .
+#
 # Uses itch.io command-line butler:
 # https://itch.io/docs/butler/installing.html
 # https://itch.io/docs/itch/installing/linux/ubuntu-and-debian.html
 # -----------------------------------------------------------------------
+
+source download_github.sh
 
 ITCH_IO_NAME='castle-engine/castle-game-engine'
 
@@ -33,12 +36,20 @@ temporary_unpack ()
   pushd .
   cd "${UPLOAD_DIR}"
 
+  # Note: we unpack with less verbose options, to not flood script
+  # with lots of output (our CGE contains many files)
+  # and thus obscure MD5 checksums (which are useful to see and compare).
+
   case "$FILE_EXTENSION" in
     .zip)
-      unzip "${FILE_NAME}"
+      unzip -q "${FILE_NAME}"
+      # verbose:
+      # unzip "${FILE_NAME}"
       ;;
     .gz) # assuming .tar.gz
-      tar xzvf "${FILE_NAME}"
+      tar xzf "${FILE_NAME}"
+      # verbose:
+      # tar xzvf "${FILE_NAME}"
       ;;
     *)
       echo "Cannot deal with file $FILE_NAME, extension $FILE_EXTENSION"
@@ -50,22 +61,37 @@ temporary_unpack ()
   popd
 }
 
-butler_push ()
+# Upload to itch.io one platform.
+# $1 - itch.io manifest file name, like "castle_game_engine-windows.itch.toml".
+#      Must be in itch_io_manifests/ directory.
+# $2, $3, $4 - arguments for download_github_release, see docs of it.
+# $@ - additional arguments for "butler push ...".
+do_upload_itch_io ()
 {
   UPLOAD_DIR=/tmp/upload_itch_io_unpacked_$$
   rm -Rf "${UPLOAD_DIR}"
   mkdir "${UPLOAD_DIR}"
 
-  local MANIFEST=itch_io_manifests/"$1"
-  local DOWNLOAD_URL="$2"
-  shift 2
+  local MANIFEST="`pwd`/itch_io_manifests/${1}"
 
-  local DOWNLOAD_URL_EXTENSION=".${DOWNLOAD_URL##*.}"
-  local FILE_NAME="downloaded${DOWNLOAD_URL_EXTENSION}"
-  wget "${DOWNLOAD_URL}" --output-document "${UPLOAD_DIR}/${FILE_NAME}"
+  # arguments for download_github_release
+  local DOWNLOAD_GIT_ORG_REPO="$2"
+  local DOWNLOAD_GIT_TAG="$3"
+  local DOWNLOAD_FILE_NAME="$4"
+  shift 4
 
-  temporary_unpack "${FILE_NAME}"
+  cd "${UPLOAD_DIR}"
+  download_github_release "${DOWNLOAD_GIT_ORG_REPO}" "${DOWNLOAD_GIT_TAG}" "${DOWNLOAD_FILE_NAME}"
+
+  echo '-------------------------------------------------------------'
+  echo 'Unpacking'
+
+  temporary_unpack "${DOWNLOAD_FILE_NAME}"
   cp -f "${MANIFEST}" "${UPLOAD_DIR}"/.itch.toml
+
+  echo '-------------------------------------------------------------'
+  echo 'Uploading'
+
   butler push "${UPLOAD_DIR}" "$@"
 
   # clean here, to not consume disk space once script exits
@@ -75,8 +101,12 @@ butler_push ()
 VERSION=`castle-engine --version | sed -e "s/^castle-engine //" `
 echo "Uploading Castle Game Engine version ${VERSION} to itch.io"
 
-butler_push castle_game_engine-windows.itch.toml https://github.com/castle-engine/castle-engine/releases/download/v"${VERSION}"/castle-engine-"${VERSION}"-win64-x86_64.zip $ITCH_IO_NAME:windows --userversion "${VERSION}"
-butler_push castle_game_engine-linux.itch.toml   https://github.com/castle-engine/castle-engine/releases/download/v"${VERSION}"/castle-engine-"${VERSION}"-linux-x86_64.zip $ITCH_IO_NAME:linux   --userversion "${VERSION}"
+# Note about platform names (:linux etc. below):
+# detection is documented on https://itch.io/docs/butler/pushing.html#channel-names .
+
+do_upload_itch_io castle_game_engine-windows.itch.toml "castle-engine/castle-engine" "v${VERSION}" "castle-engine-${VERSION}-win64-x86_64-bundle.zip"  $ITCH_IO_NAME:windows --userversion "${VERSION}"
+do_upload_itch_io castle_game_engine-unix.itch.toml    "castle-engine/castle-engine" "v${VERSION}" "castle-engine-${VERSION}-linux-x86_64-bundle.zip"  $ITCH_IO_NAME:linux   --userversion "${VERSION}"
+do_upload_itch_io castle_game_engine-unix.itch.toml    "castle-engine/castle-engine" "v${VERSION}" "castle-engine-${VERSION}-darwin-x86_64.zip"        $ITCH_IO_NAME:mac     --userversion "${VERSION}"
 
 echo 'Runnig "butler status ..." to see status:'
 butler status $ITCH_IO_NAME
