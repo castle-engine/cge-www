3a7addf68bc588f0ddd26eda23079a00add8d091
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-10-22 03:44:14 +0200

    Upgraded modern Pascal

--- htdocs/doc/modern_pascal_code_samples/hello_world.dpr
+++ htdocs/doc/modern_pascal_code_samples/hello_world.dpr
@@ -1,10 +1,11 @@
 // Just use this line in all modern FPC sources.
 {$ifdef FPC} {$mode objfpc}{$H+}{$J-} {$endif}
-// Needed for console programs on Windows,
+
+// Below is needed for console programs on Windows,
 // otherwise (with Delphi) the default is GUI program without console.
 {$ifdef MSWINDOWS} {$apptype CONSOLE} {$endif}
 
-program MyProgram; // Save this file as myprogram.dpr
+program MyProgram;
 begin
   WriteLn('Hello world!');
 end.
