96da7594632b5b70c55d7c7878ff1063efa2344e
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-12-07 13:59:24 +0000

    PointProperties docs better

--- htdocs/x3d_implementation_shape.php
+++ htdocs/x3d_implementation_shape.php
@@ -116,27 +116,28 @@ $toc = new TableOfContents(
 
   <li><p><?php echo x3d_node_link('PointProperties'); ?>.
 
-    <p>Supported fields:
+    <p>Supported fields (below we link to Pascal API of them, see X3D specification linked above for more information):
     <ul>
-      <li><p><?php echo cgeRef('TPointPropertiesNode.PointSizeScaleFactor'); ?>,
-      <li><p><?php echo cgeRef('TPointPropertiesNode.PointSizeMinValue'); ?>,
-      <li><p><?php echo cgeRef('TPointPropertiesNode.PointSizeMaxValue'); ?>.
+      <li><p><?php echo cgeRef('TPointPropertiesNode.PointSizeScaleFactor', 'PointSizeScaleFactor'); ?>,
+      <li><p><?php echo cgeRef('TPointPropertiesNode.PointSizeMinValue', 'PointSizeMinValue'); ?>,
+      <li><p><?php echo cgeRef('TPointPropertiesNode.PointSizeMaxValue', 'PointSizeMaxValue'); ?>.
     </ul>
 
     <p>The effective scaling is multiplied by
     <?php echo cgeRef('TCastleRenderOptions.PointSize'); ?>
-    which is set for the entire
+    which determines the nominal point size for the entire
     <?php echo cgeRef('TCastleScene'); ?>.
 
-    <p>TODO: Not supported yet: <code>attenuation</code>.
 
     <p>Testcases:
     <ul>
       <li><p><a href="https://github.com/castle-engine/demo-models/blob/master/x3d/point_properties.x3dv">x3d/point_properties.x3dv in demo-models</a>
-        (<a href="https://github.com/castle-engine/demo-models/blob/master/x3d/point_properties_screenshot.png">reference screenshot</a>).
+        (<a href="https://github.com/castle-engine/demo-models/blob/master/x3d/point_properties.x3dv_screenshot.png">reference screenshot</a>).
       <li><p><a href="https://www.web3d.org/x3d/content/examples/Basic/Points/ArchimedesPointPropertiesIndex.html">X3D Example Archives: Basic, Points, Archimedes Point Properties</a>.
     </ul>
-</ul>
+
+    <p>TODO: Not supported yet: <?php echo cgeRef('TPointPropertiesNode.Attenuation', 'Attenuation'); ?>
+  </ul>
 
 <?php echo $toc->html_section(); ?>
 
