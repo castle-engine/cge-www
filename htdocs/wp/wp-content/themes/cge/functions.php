0c8d7cd4bda898f0db108877a72096e55e8fe3bb
Michalis Kamburelis <michalis.kambi@gmail.com>
2026-05-05 23:11:58 +0200

    Fix for Jetpack + urvanov-syntax-highlighter in our functions.php, not as mod for urvanov-syntax-highlighter that needed to be reapplied

--- htdocs/wp/wp-content/themes/cge/functions.php
+++ htdocs/wp/wp-content/themes/cge/functions.php
@@ -313,3 +313,48 @@ function cge_automatic_updates_is_vcs_checkout()
   return false;
 }
 add_filter('automatic_updates_is_vcs_checkout', 'cge_automatic_updates_is_vcs_checkout');
+
+/* Michalis fix:
+It seems that combination of Jetpack Markdown processing +
+https://wordpress.org/plugins/urvanov-syntax-highlighter/
+is not perfect. At the end, we get newline + </code>, which is stripped by
+this plugin (urvanov-...) but additional newline remains.
+This additional newline is clearly visible in the output, esp. as the
+plugin adds line numbers, so e.g. I want to show 2-line code
+
+```delphi
+Controllers.Initialize;
+MyWalkNavigation.UseGameController;
+```
+
+on https://castle-engine.io/wp/2025/06/08/game-controllers-gamepads-joysticks-new-comfortable-api-and-example-of-walking-and-talking-using-gamepad-from-delphi-summit-2025/
+
+... but without this fix, this would be rendered as 3 lines,
+3rd line empty (but taking space, and numbered 3).
+
+Another testcase, with non-Pascal formatting:
+https://castle-engine.io/wp/2025/05/24/web-target-progress-editor-on-web-proof-of-concept-openurl-right-mouse-button-many-new-demos-creature-ai-3d-model-viewer-physics-shooting-auto-tests-kraft-and-vampyre-imaging-on-web-wa/
+
+Note: No need to strip initial <code...>, let plugin do it by
+"Remove <code> tags surrounding the shortcode content" setting on
+https://castle-engine.io/wp/wp-admin/options-general.php?page=urvanov_syntax_highlighter_settings
+
+//$content = preg_replace('/^<code>/i', '', $content);
+//$content = preg_replace('/^<code class="language-[a-zA-Z0-9_]+">/i', '', $content);
+
+We only strip ending newline + </code> better.
+
+This processing runs before Urvanov plugin (which uses priority 100).
+*/
+add_filter('the_content', function ($content) {
+    if (strpos($content, '</code>') === false) {
+        return $content;
+    }
+    return preg_replace_callback(
+        '#<pre\b[^>]*>.*?</pre>#is',
+        function ($m) {
+            return preg_replace('|\n</code>|i', '', $m[0]);
+        },
+        $content
+    );
+}, 99);
