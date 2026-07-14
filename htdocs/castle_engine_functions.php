4b6a88803b5f7ca70d883260c4f475357f815128
Michalis Kamburelis <michalis.kambi@gmail.com>
2026-07-14 18:46:21 +0000

    Direct Linux downloads to tar.xz

--- htdocs/castle_engine_functions.php
+++ htdocs/castle_engine_functions.php
@@ -2417,17 +2417,17 @@ function cge_download_engine($version, $tag)
                   '<br>Windows' .
                   '<br><span class="download_details">(x86_64)</span></a></div>'
                 .
-                '<div class="download_platform"><a class="btn btn-primary btn-lg" href="' . $download_prefix . '-linux-x86_64-bundle.zip">' .
+                '<div class="download_platform"><a class="btn btn-primary btn-lg" href="' . $download_prefix . '-linux-x86_64-bundle.tar.xz">' .
                   '<img src="' . CURRENT_URL . '/images/os_icons/linux.png" alt="Linux (64 bit, x86_64)" width="64" height="64">' .
                   '<br>Linux' .
                   '<br><span class="download_details">(x86_64)</span></a></div>'
                 .
-                '<div class="download_platform"><a class="btn btn-primary btn-lg" href="' . $download_prefix . '-linux-arm-bundle.zip">' .
+                '<div class="download_platform"><a class="btn btn-primary btn-lg" href="' . $download_prefix . '-linux-arm-bundle.tar.xz">' .
                   '<img src="' . CURRENT_URL . '/images/os_icons/raspberry_pi_32.png" alt="Raspberry Pi 32-bit (Linux Arm)" width="91" height="64">' .
                   '<br>Raspberry Pi' .
                   '<br><span class="download_details">(Linux Arm32)</span></a></div>'
                 .
-                '<div class="download_platform"><a class="btn btn-primary btn-lg" href="' . $download_prefix . '-linux-aarch64-bundle.zip">' .
+                '<div class="download_platform"><a class="btn btn-primary btn-lg" href="' . $download_prefix . '-linux-aarch64-bundle.tar.xz">' .
                   '<img src="' . CURRENT_URL . '/images/os_icons/raspberry_pi_64.png" alt="Raspberry Pi 64-bit (Linux Aarch64)" width="91" height="64">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="' . CURRENT_URL . '/images/os_icons/pine64.png" alt="PINE64 (PineTab2, Linux Aarch64)" width="48" height="64">' .
                   '<br>Raspberry Pi, PineTab2' .
                   '<br><span class="download_details">(Linux, Arm64 aka Aarch64)</span></a></div>'
@@ -2442,7 +2442,7 @@ function cge_download_engine($version, $tag)
                   '<br>macOS' .
                   '<br><span class="download_details">(x86_64, Intel)</span></a></div>'
                 .
-                '<div class="download_platform"><a class="btn btn-primary btn-lg" href="https://castle-engine.io/compiling_from_source.php">' .
+                '<div class="download_platform"><a class="btn btn-primary btn-lg" href="https://castle-engine.io/compiling_from_source">' .
                   '<img src="' . CURRENT_URL . '/images/os_icons/freebsd.png" alt="FreeBSD (Sources)" width="64" height="64">' .
                   '<br>FreeBSD' .
                   '<br><span class="download_details">(Use the Source Luke)</span></a></div>'
